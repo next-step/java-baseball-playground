1f794e804efad867f2aada0535d515fa78043f4d
codesweaver <codetrain999@gmail.com>
2021-05-28 14:29:38 +0900

    2.문자열 야구게임. 입력값 비교. - 난수와 입력값 비교하여 일치 불일치 표기

--- src/main/java/study/StringBaseball.java
+++ src/main/java/study/StringBaseball.java
@@ -1,16 +1,54 @@
 package study;
 
-import org.w3c.dom.ranges.Range;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class StringBaseball {
     public static void main(String[] args) {
         List<Integer> numbers = getNumbers();
-        String[] inputs = getUserInput();
+        int[] results;
+        do {
+            List<Integer> inputs = toIntegerList(getUserInput());
+            results = getResult(numbers, inputs);
+            printResults(results);
+        } while (!isCorrect(results));
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임종료.");
+    }
+
+    private static void printResults(int[] results) {
+        System.out.printf("%d 아웃, ", results[0]);
+        System.out.printf("%d 볼, ", results[1]);
+        System.out.printf("%d 스트라이크\n", results[2]);
+    }
+
+    private static boolean isCorrect(int[] results) {
+        return results[2] == 3;
+    }
+
+    private static int[] getResult(List<Integer> numbers, List<Integer> inputs) {
+        int[] results = new int[3];
+        for (int i = 0; i < inputs.size(); i++) {
+            results[getCompareResult(numbers, inputs, i)]++;
+        }
+        return results;
+    }
+
+    private static int getCompareResult(List<Integer> numbers, List<Integer> inputs, int i) {
+        int result = 0;
+        if (numbers.contains(inputs.get(i))) {
+            result = 1;
+            if (numbers.get(i).equals(inputs.get(i))) {
+                result = 2;
+            }
+        }
+        return result;
+    }
+
+    private static List<Integer> toIntegerList(String[] inputs) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String input : inputs) {
+            numbers.add(Integer.parseInt(input));
+        }
+        return numbers;
     }
 
     private static String[] getUserInput() {

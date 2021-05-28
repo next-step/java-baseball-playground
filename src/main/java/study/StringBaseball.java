212fed880332892215051cec6a3a0d67bff57a6c
codesweaver <codetrain999@gmail.com>
2021-05-28 12:00:31 +0900

    2.문자열 야구게임. 사용자 입력. - 사용자로부터 문자열 입력.

--- src/main/java/study/StringBaseball.java
+++ src/main/java/study/StringBaseball.java
@@ -5,13 +5,18 @@ import org.w3c.dom.ranges.Range;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class StringBaseball {
     public static void main(String[] args) {
         List<Integer> numbers = getNumbers();
-        for (Integer number : numbers) {
-            System.out.println(number);
-        }
+        String[] inputs = getUserInput();
+    }
+
+    private static String[] getUserInput() {
+        System.out.print("숫자를 입력해주세요: ");
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine().split("");
     }
 
     private static List<Integer> getNumbers() {

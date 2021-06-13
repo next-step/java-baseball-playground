b9971d9eee2ac32bd9b60c443569a376be6d1dfc
kimyoonje <forteas2003@hanmail.net>
2021-06-14 00:51:34 +0900

    feat(BaseBall) : 볼 판정 기능

--- src/main/java/baseball/BaseBall.java
+++ src/main/java/baseball/BaseBall.java
@@ -12,10 +12,16 @@ public class BaseBall {
 	//값이 같아야함
 	//포지션이 같아야함
 	//스트라이크가 아니라면 볼이다.
-	public boolean isStrike(int position, String playerBall) {
-		if ( computerBallList.get(position).getValue() == playerBall ) {
-			return true;
-		}
-		return false;
+	public boolean isStrike(Ball playerBall) {
+		return computerBallList.stream()
+				.filter(one -> one.getValue().equals(playerBall.getValue()) )
+				.anyMatch(one -> one.getPosition() == playerBall.getPosition());
+	}
+	
+	//포지션은 다르지만 값은 포함되어있다
+	public boolean isBall(Ball playerBall) {
+		return computerBallList.stream()
+				.filter(one -> one.getValue().equals(playerBall.getValue()) )
+				.anyMatch(one -> one.getPosition() != playerBall.getPosition());
 	}
 }

536084cec1e3c73cef837c585d0c2031b78aef21
Jiyeon Rhea Park <rheazen@gmail.com>
2021-07-12 17:26:34 +0900

    FEAT redo

--- src/main/java/baseball/BaseballMain.java
+++ src/main/java/baseball/BaseballMain.java
@@ -1,30 +1,32 @@
 package baseball;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class BaseballMain {
 
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame();
         List<Integer> me = BaseballView.askGameInput();
-        if (baseballGame.isStrike(me) == 3) {
-            BaseballView.showGameResult(baseballGame, me);
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            Scanner scanner = new Scanner(System.in);
-            String closeInput = scanner.nextLine();
-            if (closeInput.equals("1")) {
-                BaseballGame baseballGame1 = new BaseballGame();
+            while (baseballGame.isStrike(me) != 3) {
+                BaseballView.showGameResult(baseballGame, me);
+                me = BaseballView.askGameInput();
+
+                if (baseballGame.isStrike(me) == 3) {
+                    BaseballView.showGameResult(baseballGame, me);
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    Scanner scanner = new Scanner(System.in);
+                    String closeInput = scanner.nextLine();
+                    if (closeInput.equals("1")) {
+                        //reStart main function!
+                        BaseballMain.main(args);
+                    }
+                    if (closeInput.equals("2")) {
+                    } else {
+                        throw new IllegalArgumentException("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+                    }
+                }
             }
-        }
-        while (baseballGame.isStrike(me) != 3) {
-            BaseballView.showGameResult(baseballGame, me);
-            me = BaseballView.askGameInput();
-//            BaseballView.showGameResult(baseballGame, me);
+
         }
     }
-}
--- src/main/java/baseball/BaseballView.java
+++ src/main/java/baseball/BaseballView.java
@@ -1,2 +1,53 @@
-package baseball;public class BaseballView {
+package baseball;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class BaseballView {
+
+    private BaseballView(){}
+    //String.format not in System.out.println : build error
+    //여기에서 showGameResult 재호출 시 무한loop
+
+    public static List<Integer> askGameInput() {
+        System.out.println("숫자를 입력해 주세요 :");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        List<Integer> gameInput = checkInput(input);
+        return gameInput;
+    }
+
+    private static List<Integer> checkInput(String input) {
+        if (input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("입력값 없습니다");
+        }
+        if (input.length() != 3) {
+            throw new IllegalArgumentException("세 자릿수 아닙니다");
+        }
+        return Arrays.stream(input.split(""))
+                .filter(o -> Pattern.matches("^[1-9]*$", o))
+                .map(o -> Integer.parseInt(o))
+                .collect(Collectors.toList());
+    }
+
+    public static void showGameResult(BaseballGame baseballGame, List<Integer> me) {
+        if (baseballGame.isStrike(me) == 0 && baseballGame.isBall(me) == 0) {
+            System.out.println("포볼");
+        }
+        if (baseballGame.isStrike(me) == 0) {
+            System.out.println(baseballGame.isBall(me) + "볼");
+        }
+        if (baseballGame.isStrike(me) == 3) {
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        }
+        if (baseballGame.isBall(me) == 0 && baseballGame.isStrike(me) != 3) {
+            System.out.println(baseballGame.isStrike(me) + "스트라이크");
+        }
+        if (baseballGame.isBall(me) != 0 && baseballGame.isStrike(me) != 0) {
+            System.out.println(baseballGame.isBall(me) + "볼 " + baseballGame.isStrike(me) + "스트라이크");
+        }
+    }
 }

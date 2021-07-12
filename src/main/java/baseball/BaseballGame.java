bb7554b1f797f4a260d0b993798a3a4a848909e5
Jiyeon Rhea Park <rheazen@gmail.com>
2021-07-12 13:53:53 +0900

    FEAT balls

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,7 +2,7 @@ package baseball;
 
 public class BaseballGame {
     //String.charAt(int index)
-    public static int play(String me, String you) {
+    public static int isStrike(String me, String you) {
         int strikeCount = 0;
         for (int i = 0; i < 3; i++) {
             if (me.charAt(i) == you.charAt(i)) {
@@ -11,4 +11,16 @@ public class BaseballGame {
         }
         return strikeCount;
     }
+
+    public static int isBall(String me, String you) {
+        int ballCount = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (me.charAt(i) == you.charAt(j) && i != j) {
+                    ballCount += 1;
+                }
+            }
+        } return ballCount;
+    }
+
 }
--- src/test/java/baseball/BaseballGameTest.java
+++ src/test/java/baseball/BaseballGameTest.java
@@ -13,7 +13,7 @@ public class BaseballGameTest {
     @ParameterizedTest
     @CsvSource(value = {"123:123", "987:987"}, delimiter = ':')
     void true_3Strike(String me, String you) {
-        assertThat(BaseballGame.play(me,you)).isEqualTo(3);
+        assertThat(BaseballGame.isStrike(me,you)).isEqualTo(3);
     }
 
     // num => "num스트라이크" 는 view에서 진행
@@ -21,6 +21,20 @@ public class BaseballGameTest {
     @ParameterizedTest
     @CsvSource(value = {"123:156", "987:127"}, delimiter = ':')
     void true_1Strike(String me, String you){
-        assertThat(BaseballGame.play(me,you)).isEqualTo(1);
+        assertThat(BaseballGame.isStrike(me,you)).isEqualTo(1);
+    }
+
+    @DisplayName("1 Ball")
+    @ParameterizedTest
+    @CsvSource(value = {"145:713", "671:213"}, delimiter = ':')
+    void true_1Ball(String me, String you){
+        assertThat(BaseballGame.isBall(me,you)).isEqualTo(1);
+    }
+
+    @DisplayName("2 Ball")
+    @ParameterizedTest
+    @CsvSource(value = {"471:713", "341:213"}, delimiter = ':')
+    void true_2Ball(String me, String you){
+        assertThat(BaseballGame.isBall(me,you)).isEqualTo(2);
     }
 }

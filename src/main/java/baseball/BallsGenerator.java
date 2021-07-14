0f6203d089a9a435767b9784fe934da710c9eb25
Jiyeon Rhea Park <rheazen@gmail.com>
2021-07-14 22:39:19 +0900

    FEAT ballsAuto, ballsManual

--- src/main/java/baseball/BallsGenerator.java
+++ src/main/java/baseball/BallsGenerator.java
@@ -9,39 +9,26 @@ import static baseball.BallNum.BALLS_POOL;
 
 public class BallsGenerator {
 
+
     private final List<Ball> systemBallsList;
 
     public BallsGenerator(List<Ball> systemBallsList) {
         this.systemBallsList = systemBallsList;
     }
 
-//    public BallsGenerator() {
-//        this.systemBallsList = makeRandomBalls();
-//    }
-//
-//
-//    private List<Ball> makeRandomBalls() {
-//        Collections.shuffle(BALLS_POOL);
-//        return Arrays.asList(new Ball(new BallNum(BALLS_POOL.get(0)), 1),
-//                new Ball(new BallNum(BALLS_POOL.get(1)), 2),
-//                new Ball(new BallNum(BALLS_POOL.get(2)), 3);
-//    }
+    public BallsGenerator() {
+         this.systemBallsList = makeRandomBalls();
+    }
+
+    private List<Ball> makeRandomBalls() {
+        Collections.shuffle(BALLS_POOL);
+        return Arrays.asList(new Ball(new BallNum(BALLS_POOL.get(0)), 1),
+                new Ball(new BallNum(BALLS_POOL.get(1)), 2),
+                new Ball(new BallNum(BALLS_POOL.get(2)), 3));
+    }
 
     public List<Ball> getSystemBallsList() {
         return systemBallsList;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        BallsGenerator that = (BallsGenerator) o;
-        return Objects.equals(systemBallsList, that.systemBallsList);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(systemBallsList);
     }
 
 }

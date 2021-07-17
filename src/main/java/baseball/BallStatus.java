798d710fd4d484f146535a1cd71e5f9afdbd727c
Jiyeon Rhea Park <rheazen@gmail.com>
2021-07-18 01:45:09 +0900

    TEST update BallStatusRecord

--- src/main/java/baseball/BallStatus.java
+++ src/main/java/baseball/BallStatus.java
@@ -1,5 +1,8 @@
 package baseball;
 
+import java.util.Arrays;
+import java.util.HashMap;
+
 public enum BallStatus {
     STRIKE,
     BALL,
--- src/main/java/baseball/BallStatusRecord.java
+++ src/main/java/baseball/BallStatusRecord.java
@@ -1,28 +1,48 @@
 package baseball;
 
-import java.util.HashMap;
+import java.util.*;
 
-public class BallStatusMap {
+public class BallStatusRecord {
 
-    private final HashMap<BallStatus, Integer> ballStatusMap;
+    private final HashMap<BallStatus, Integer> ballStatusRecord;
 
-    BallStatusMap(BallStatus ballStatus) {
-        this.ballStatusMap = makeInitMap(ballStatus);
+    BallStatusRecord(BallStatus ballStatus) {
+        this.ballStatusRecord = makeInitRecord(ballStatus);
     }
 
-    public HashMap<BallStatus, Integer> makeInitMap(BallStatus ballStatus) {
+    public HashMap<BallStatus, Integer> makeInitRecord(BallStatus ballStatus) {
         HashMap<BallStatus, Integer> initMap = new HashMap<>();
         initMap.put(ballStatus, 0);
         return initMap;
     }
 
-    public int countOf(BallStatus ballStatus) {
-        if (this.ballStatusMap.containsKey(ballStatus)) {
-            return this.ballStatusMap.get(ballStatus);
+
+    public void updateCountOf(BallStatus ballStatus, int count) {
+        if (hasStatus(ballStatus)) {
+            ballStatusRecord.put(ballStatus, findCountOf(ballStatus) + count);
         }
-        return 0;
+    }
+
+    public boolean hasStatus(BallStatus ballStatus) {
+        return ballStatusRecord.containsKey(ballStatus);
+    }
+
+    public int findCountOf(BallStatus ballStatus) {
+            return this.ballStatusRecord.get(ballStatus);
     }
 
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BallStatusRecord that = (BallStatusRecord) o;
+        return Objects.equals(ballStatusRecord, that.ballStatusRecord);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ballStatusRecord);
+    }
 }

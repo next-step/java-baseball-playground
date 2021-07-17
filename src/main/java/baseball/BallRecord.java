453424969fe5f50b9962a6924bff05815d1d43df
Jiyeon Rhea Park <rheazen@gmail.com>
2021-07-17 20:24:54 +0900

    FEAT updateBallRecord delimiter void

--- src/main/java/baseball/BallRecord.java
+++ src/main/java/baseball/BallRecord.java
@@ -6,14 +6,33 @@ import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
 
+//Record owns Game, not the other way
 public class BallRecord {
 
     private final List<HashMap<BallStatus,Integer>> statusMapList;
+    private final BallGame ballGame;
 
-    BallRecord() {
+    BallRecord(BallGame ballGame) {
         this.statusMapList = makeStatusMapList();
+        this.ballGame = ballGame;
     }
 
+//    //명령 질의 디미터 (실행은 조회값없음)
+    public void updateBallRecord(UserBalls userBalls) {
+       statusMapList.forEach( o -> updateStatusMap(o, userBalls));
+    }
+
+    private void updateStatusMap(HashMap<BallStatus, Integer> map, UserBalls userBalls) {
+        if (map.containsKey(BallStatus.STRIKE)) {
+            map.put(BallStatus.STRIKE, map.get(BallStatus.STRIKE) + ballGame.countMatchToStrike(userBalls));
+        }
+        if (map.containsKey(BallStatus.BALL)) {
+            map.put(BallStatus.BALL, map.get(BallStatus.BALL) + ballGame.countMatchToBall(userBalls));
+        }
+        if (map.containsKey(BallStatus.NOTHING)) {
+            map.put(BallStatus.NOTHING, map.get(BallStatus.NOTHING) + ballGame.countMatchToNothing(userBalls));
+        }
+    }
 
     //Optional Stream 다시 고민
     public int findBallStatusCount(BallStatus ballStatus) {
@@ -49,11 +68,12 @@ public class BallRecord {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         BallRecord that = (BallRecord) o;
-        return Objects.equals(statusMapList, that.statusMapList);
+        return Objects.equals(getStatusMapList(), that.getStatusMapList()) &&
+                Objects.equals(ballGame, that.ballGame);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(statusMapList);
+        return Objects.hash(getStatusMapList(), ballGame);
     }
 }
--- src/test/java/baseball/BallRecordTest.java
+++ src/test/java/baseball/BallRecordTest.java
@@ -1,16 +1,33 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallRecordTest {
 
+    private BallGame ballGame;
+    //as a private field and add
+    //why not work?
+
+    @BeforeEach
+    void setUp(){
+        BallsGenerator ballsGenerator = new BallsGenerator(Arrays.asList(
+                new Ball(new BallNum("3"), 1),
+                new Ball(new BallNum("5"), 2),
+                new Ball(new BallNum("7"), 3)
+        ));
+        ballGame = new BallGame(ballsGenerator);
+    }
+
     @DisplayName("Empty BallRecord")
     @Test
-    void is_EmptyBallRecord(){
-        BallRecord ballRecord = new BallRecord();
+    void is_Empty_BallRecord(){
+        BallRecord ballRecord = new BallRecord(ballGame);
         assertThat(ballRecord.findBallStatusCount(BallStatus.STRIKE)).isEqualTo(0);
         assertThat(ballRecord.findBallStatusCount(BallStatus.BALL)).isEqualTo(0);
     }

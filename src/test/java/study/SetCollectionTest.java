0ba0f360880bfb4ada9720a6c067380e28bbddcd
Leeho <ho.sol.lee@gmail.com>
2021-08-08 14:16:32 +0900

    [210808] Learning Test : Set Collection

--- src/test/java/study/SetCollectionTest.java
+++ src/test/java/study/SetCollectionTest.java
@@ -1,12 +1,16 @@
 package study;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class SetCollectionTest {
 
@@ -30,4 +34,26 @@ public class SetCollectionTest {
 	void getSizeByHasSize() {
 		assertThat(numbers).hasSize(3);
 	}
+
+	@Test
+	void isContainOneToThree() {
+		assertThat(numbers).contains(1);
+		assertThat(numbers).contains(2);
+		assertThat(numbers).contains(3);
+
+	}
+
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3}) // go to Parameter
+	void isContainOneToThreeCheckTrue(int valueSource) {
+		assertTrue(numbers.contains(valueSource));
+	}
+
+	@ParameterizedTest
+	@CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+	void isContainOneToThreeCheckTrueOrFalse(String number, String expected) {
+		String isContain = numbers.contains(Integer.parseInt(number)) ? "true" : "false";
+		assertEquals(expected, isContain);
+	}
+
 }

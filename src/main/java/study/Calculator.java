1a157dd6ee9f07931a1301f8a3051e432c324ea0
Leeho <ho.sol.lee@gmail.com>
2021-08-10 22:08:08 +0900

    [210810] Calculator v2.0
    문자열 계산기에 필요한 프로덕션 코드와 테스트 코드 일부 작성

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,6 +1,11 @@
 package study;
 
+import java.util.Scanner;
+
 public class Calculator {
+
+	int answer;
+	Scanner scanner = new Scanner(System.in);
 
 	/*
 	 * 필요한 기능
@@ -8,4 +13,22 @@ public class Calculator {
 	 * 2. split 된 문자열을 제대로 parseInt로 변환하는지
 	 * 3. 계산을 정상적으로 수행하는지
 	 */
+
+	public void Start() {
+		String formula = scanner.nextLine();
+
+		String[] numberAndOperator = split(formula);
+	}
+
+	public String[] split(String nums) {
+		return nums.split(" ");
+	}
+
+	public void operate(int nums, String operator) {
+
+	}
+
+	public static void main(String[] args) {
+		Calculator cal = new Calculator();
+	}
 }
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -2,12 +2,34 @@ package study;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CalculatorTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	Calculator cal;
+
+	@BeforeAll
+	void setUp() {
+		cal = new Calculator();
 	}
+
+	@Test
+	void start() {
+		String fomular = cal.scanner.nextLine();
+	}
+
+	@ParameterizedTest
+	@ValueSource(strings = "1 + 2 / 3 - 4") // go to Parameter
+	void splitTest(String nums) {
+		Calculator cal = new Calculator();
+
+		String[] actual = cal.split(nums);
+		String[] expected = {"1", "+", "2", "/", "3", "-", "4"};
+
+		assertArrayEquals(expected, actual);
+	}
+
 }

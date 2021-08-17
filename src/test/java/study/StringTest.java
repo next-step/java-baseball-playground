0a92d65b18badf793a42918a04c30c078196a03b
Hyunta <mohai2618@naver.com>
2021-08-17 13:18:30 +0900

    test: Add splitTest

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,8 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
@@ -9,5 +11,14 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String[] oneTwo = "1,2".split(",");
+        System.out.println("oneTwo = " + Arrays.toString(oneTwo));
+        assertThat(oneTwo).contains("1");
+        assertThat(oneTwo).contains("2");
+        assertThat(oneTwo).containsExactly("1", "2");
     }
 }

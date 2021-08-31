ea315f28f4e3d1f265794543353284358f05d69f
sdrs6921 <sdrs6921@naver.com>
2021-09-01 01:14:33 +0900

    refactor : GameStatus 문자열로 변경

--- src/main/java/view/GameConsoleView.java
+++ src/main/java/view/GameConsoleView.java
@@ -18,7 +18,7 @@ public class GameConsoleView implements GameView {
 
     public GameStatus gameInput() {
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-        int status = scanner.nextInt();
+        String status = scanner.nextLine();
         return GameStatus.from(status);
     }
 }
--- src/test/java/domain/GameStatusTest.java
+++ src/test/java/domain/GameStatusTest.java
@@ -13,14 +13,14 @@ class GameStatusTest {
     @ParameterizedTest
     @CsvSource(value = {"PLAY,1", "OVER,2"})
     @DisplayName("게임의 상태를 반환한다")
-    void status(GameStatus gameStatus, int status) {
+    void status(GameStatus gameStatus, String status) {
         assertThat(gameStatus).isEqualTo(GameStatus.from(status));
     }
 
     @Test
     @DisplayName("올바르지 않은 게임 상태는 예외를 발생시킨다")
     void from_throw_exception_with_invalid_status() {
-        int invalidStatus = 3;
+        String invalidStatus = "3";
 
         assertThatIllegalArgumentException().isThrownBy(() -> GameStatus.from(invalidStatus))
                 .withMessage("잘못된 게임의 상태입니다");

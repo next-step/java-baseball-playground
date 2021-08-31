9e5fd66111632347985a3e2a5c5777fdbb86515b
sdrs6921 <sdrs6921@naver.com>
2021-09-01 01:15:47 +0900

    refactor : Main 메서드 수정

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import controller.GameController;
 import controller.RoundController;
 import domain.GameStatus;
+import domain.Generation;
+import domain.RandomNumberGeneration;
 import view.GameView;
 import view.GameConsoleView;
 import view.RoundInputConsoleView;
@@ -8,16 +10,19 @@ import view.RoundInputView;
 import view.RoundOutputConsoleView;
 import view.RoundOutputView;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        final Scanner scanner = new Scanner(System.in);
-        final RoundInputView roundInputView = RoundInputConsoleView.from(scanner);
-        final GameView gameView = GameConsoleView.from(scanner);
-        final RoundOutputView roundOutputView = RoundOutputConsoleView.getInstance();
-        final RoundController roundController = RoundController.of(roundInputView, roundOutputView);
-        final GameController gameController = GameController.of(roundController, gameView, GameStatus.PLAY);
+        Scanner scanner = new Scanner(System.in);
+        RoundInputView roundInputView = RoundInputConsoleView.from(scanner);
+        GameView gameView = GameConsoleView.from(scanner);
+        RoundOutputView roundOutputView = RoundOutputConsoleView.getInstance();
+        Random random = new Random();
+        Generation generation = new RandomNumberGeneration(random);
+        RoundController roundController = RoundController.of(roundInputView, roundOutputView, generation);
+        GameController gameController = GameController.of(roundController, gameView, GameStatus.PLAY);
         gameController.run();
         scanner.close();
     }

9815f32871cfc0121b433fbbaf54744c6c43c238
Charles <chkftm12@gmail.com>
2021-09-15 09:46:42 +0900

    add test split

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,19 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    
+    @Test
+    void split() {
+        String[] numbers = "1,2".split(",");
+        assertThat(numbers).contains("1", "2");        
+    }
+    
+    @Test
+    void split2() {
+        String[] nums = "1".split(",");
+        for(String num: nums) {
+            System.out.println(num);
+        }
+        assertThat(nums).containsExactly("1");
+    }
 }

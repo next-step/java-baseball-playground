34986f216db53ec7dcd4f3f0577722ead834da99
growinghsb <seungbeombm@gmail.com>
2021-09-20 18:43:13 +0900

    [MOD] StringCalculator.class in main delete & add Test

--- src/main/java/StringCalculator.java
+++
@@ -1,5 +0,0 @@
-public class StringCalculator {
-    public static void main(String[] args) {
-
-    }
-}
---
+++ src/test/java/study/StringCalculator.java
@@ -0,0 +1,13 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class StringCalculator {
+
+    @Test
+    void stringCalculator() {
+        Scanner sc = new Scanner(System.in);
+    }
+}

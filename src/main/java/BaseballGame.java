a5f1c776609e19de006ec724485c540a6c1654ee
growinghsb <seungbeombm@gmail.com>
2021-09-21 16:33:26 +0900

    [ADD] required 1 finish

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -1,3 +1,36 @@
+import java.util.Random;
+
 public class BaseballGame {
 
+    public int[] generateNumbers() {
+        int digit = 3; // 3개 뽑을 것이고, 3번 반복할 횟수를 지정
+        int[] numbers = new int[digit];
+
+        int count = 0; // 배열의 인덱스 체크, 값이 들어갔을 때만 + 1 하게 된다.
+        while (count != digit) {
+            count = createRandomNums(numbers, count);
+        }
+        return numbers;
+    }
+
+    private boolean duplicateCheck(int[] numbers, int num) {
+        for (int number : numbers) {
+            if (number == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private int createRandomNums(int[] numbers, int count) {
+        Random random = new Random();
+
+        int num = random.nextInt(9) + 1;
+        if (duplicateCheck(numbers, num)) {
+            numbers[count] = num;
+            count++;
+        }
+
+        return count;
+    }
 }
--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -1,5 +1,25 @@
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BaseballGameTest {
+    BaseballGame baseballGame;
+
+    @BeforeEach
+    void setUp() {
+        baseballGame = new BaseballGame();
+    }
+
+    @Test
+    void generateNumbersTest() {
+        int[] computerNums = baseballGame.generateNumbers();
+
+        assertThat(computerNums.length).isEqualTo(3);
+        assertThat(computerNums).doesNotHaveDuplicates();
+        assertThat(computerNums).doesNotContain(0);
+    }
 
 }

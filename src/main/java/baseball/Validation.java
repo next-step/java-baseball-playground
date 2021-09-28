69588825b529e8bec89e516e25e493864c5699c2
sukeun1997 <busan-sy@naver.com>
2021-09-28 15:53:32 +0900

    feat(baseball) : 야구 숫자 중복확인

--- src/main/java/baseball/Validation.java
+++ src/main/java/baseball/Validation.java
@@ -1,2 +1,17 @@
-package baseball;public class Validation {
+package baseball;
+
+import java.util.List;
+
+public class Validation {
+
+
+    public static boolean confirmNumber(int i) {
+        return i >= 1 && i <= 9;
+
+    }
+
+    public static boolean confirmDuplication(List<Integer> request) {
+        //false : 중복 o , true : 중복 x
+        return request.stream().distinct().count() != request.size();
+    }
 }
--- src/test/java/baseball/ValidationTest.java
+++ src/test/java/baseball/ValidationTest.java
@@ -1,2 +1,26 @@
-package baseball;public class ValidationTest {
+package baseball;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ValidationTest {
+
+    @Test
+    void 야구_숫자_1_9_확인() {
+        assertThat(Validation.confirmNumber(1)).isEqualTo(true);
+        assertThat(Validation.confirmNumber(0)).isEqualTo(false);
+        assertThat(Validation.confirmNumber(9)).isEqualTo(true);
+        assertThat(Validation.confirmNumber(10)).isEqualTo(false);
+    }
+
+    @Test
+    void 야구_중복_숫자_확인() {
+        assertThat(Validation.confirmDuplication(Arrays.asList(1, 2, 2))).isEqualTo(true);
+        assertThat(Validation.confirmDuplication(Arrays.asList(2, 3, 2))).isEqualTo(true);
+        assertThat(Validation.confirmDuplication(Arrays.asList(3, 3, 2))).isEqualTo(true);
+        assertThat(Validation.confirmDuplication(Arrays.asList(1, 2, 3))).isEqualTo(false);
+    }
 }

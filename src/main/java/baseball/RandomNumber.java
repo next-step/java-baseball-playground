56c61ec52277e39ac586ae131723db92ba8f7560
Seokho-Ham <seokho9522@gmail.com>
2021-10-08 14:46:28 +0900

    feat(baseball) - Application, User 클래스 구현 및 기능 완성

--- src/main/java/baseball/RandomNumber.java
+++ src/main/java/baseball/RandomNumber.java
@@ -1,15 +1,25 @@
 package baseball;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class RandomNumber {
 
+    public static List<Integer> makeFullRandomNumber(){
+        List<Integer> answer = new ArrayList<>();
+
+        while(answer.size()!=3){
+            answer.add(makeRandomNumber(answer));
+        }
+        return answer;
+    }
+
     public static int makeRandomNumber(List<Integer> balls) {
         Random random = new Random();
         int result = random.nextInt(9) + 1;
 
-        while (!checkRange(result) && !checkOverlap(balls, result)) {
+        while (!checkRange(result) || balls.contains(result)) {
             result = random.nextInt(9) + 1;
         }
 
@@ -27,10 +37,4 @@ public class RandomNumber {
         return false;
     }
 
-    public static boolean checkOverlap(List<Integer> balls, int number) {
-        if (balls.contains(number)) {
-            return false;
-        }
-        return true;
-    }
 }
--- src/test/java/baseball/RandomNumberTest.java
+++ src/test/java/baseball/RandomNumberTest.java
@@ -22,13 +22,4 @@ public class RandomNumberTest {
         assertThat(RandomNumber.checkRange(10)).isFalse();
     }
 
-    @Test
-    @DisplayName("중복 값 테스트")
-    void checkOverlap(){
-
-        assertThat(RandomNumber.checkOverlap(Arrays.asList(1,2,3), 1)).isFalse();
-        assertThat(RandomNumber.checkOverlap(Arrays.asList(1,2,3), 2)).isFalse();
-        assertThat(RandomNumber.checkOverlap(Arrays.asList(1,2,3), 4)).isTrue();
-
-    }
 }

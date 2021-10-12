253e340a90464d42942e12961ab26947bf803e10
Seokho-Ham <seokho9522@gmail.com>
2021-10-12 23:36:53 +0900

    refactoring(application) - Application production code 작성

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,34 @@
+package baseball;
+
+public class Application {
+
+
+    public void run(){
+        Balls answerBalls = new Balls(RandomNumberCreator.makeNumberList());
+        User user = new User();
+        boolean runStatus = true;
+        while(runStatus){
+            String input = user.getInput();
+            Balls userBalls = new Balls(input);
+            runStatus = !Referee.judgement(answerBalls, userBalls);
+        }
+        if(restart()){
+            run();
+        }
+
+    }
+    private boolean restart(){
+        User user = new User();
+        String decision = user.askRestart();
+        if(decision.equals("1")){
+            return true;
+        }
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+        Application computer = new Application();
+        computer.run();
+    }
+}

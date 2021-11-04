4f18f02b30f1369be0b86ec72ceab98250ef0f6d
Korea-subinpark <subinpark.re@gmail.com>
2021-11-04 21:48:24 +0900

    [test] 통합 테스트

--- src/main/java/baseball/Main.java
+++ src/main/java/baseball/Main.java
@@ -1,4 +1,36 @@
 package baseball;
 
+import baseball.domain.Balls;
+import baseball.domain.GameResult;
+import baseball.service.RandomGenerator;
+import baseball.view.InputView;
+import baseball.view.ResultView;
+
 public class Main {
+
+    public static void main(String[] args) {
+        int gameStart = 1;
+
+        while (gameStart == 1) {
+            Balls randomBalls = RandomGenerator.getRandomBalls();
+
+            while (true) {
+                String inputNumber = InputView.inputNumber();
+
+                GameResult result = randomBalls.play(new Balls(inputNumber));
+                if (result.isEnd()) {
+                    System.out.println(ResultView.endMessage());
+                    System.out.println(ResultView.restartMessage());
+                    String restartInput = InputView.input();
+
+                    if (!restartInput.equals("1")) {
+                        gameStart = 2;
+                    }
+                    break;
+                }
+
+                System.out.println(ResultView.result(result));
+            }
+        }
+    }
 }

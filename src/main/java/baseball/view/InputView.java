ad67e460e650e30a62865940584548afd8e9bfbc
Korea-subinpark <subinpark.re@gmail.com>
2021-11-04 21:30:17 +0900

    [refactor] inputView static으로 변경

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -3,13 +3,12 @@ package baseball.view;
 import java.util.Scanner;
 
 public class InputView {
-    private Scanner sc;
 
-    public InputView() {
-        sc = new Scanner(System.in);
+    private InputView() {
     }
 
-    public String input() {
+    public static String input() {
+        Scanner sc = new Scanner(System.in);
         System.out.print("숫자를 입력해 주세요 : ");
         return sc.next();
     }

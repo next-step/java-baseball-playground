4d9f2c0c72f3761c1bd1c6813e2ae6ca454a9017
Korea-subinpark <subinpark.re@gmail.com>
2021-11-04 21:21:16 +0900

    [feat] view 구현

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,16 @@
 package baseball.view;
 
+import java.util.Scanner;
+
 public class InputView {
+    private Scanner sc;
+
+    public InputView() {
+        sc = new Scanner(System.in);
+    }
+
+    public String input() {
+        System.out.print("숫자를 입력해 주세요 : ");
+        return sc.next();
+    }
 }
--- src/main/java/baseball/view/ResultView.java
+++ src/main/java/baseball/view/ResultView.java
@@ -1,4 +1,31 @@
 package baseball.view;
 
+import baseball.domain.GameResult;
+
 public class ResultView {
+
+    private ResultView() {
+    }
+
+    public static String result(GameResult result) {
+        String resultMessage = "";
+
+        if (result.getBall() != 0) {
+            resultMessage = result.getBall() + "볼";
+        }
+
+        if (result.getStrike() != 0) {
+            resultMessage += " " + result.getStrike() + "스트라이크";
+        }
+
+        return resultMessage;
+    }
+
+    public static String endMessage() {
+        return "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+    }
+
+    public static String restartMessage() {
+        return "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    }
 }

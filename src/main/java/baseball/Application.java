ff57def2c458fe20b631d9747d1ddb0429a378cb
kimds989 <kimds989@naver.com>
2021-12-08 00:51:28 +0900

    feat(baseball) NumberGenerator 클래스 및 메서드 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,9 +1,13 @@
 package baseball;
 
+import baseball.domain.NumberGenerator;
+
 import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
+        NumberGenerator numberGenerator = new NumberGenerator();
+        ArrayList<Integer> comNums = numberGenerator.makeThreeNumbersByCom();
     }
 
     public static ArrayList<Integer> receiveInputFromUser() {
--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -1,9 +1,15 @@
 package baseball.domain;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 public class NumberGenerator {
     public ArrayList<Integer> makeThreeNumbersByCom() {
-        return null;
+        ArrayList<Integer> comNums = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            int num = new Random().nextInt(9) + 1;
+            comNums.add(num);
+        }
+        return comNums;
     }
 }

381ce37339082426b3ed5d145640257ea1156eb4
kimds989 <kimds989@naver.com>
2021-12-08 01:41:17 +0900

    fix(baseball) Judgement findSameNumbers 오류 수정

--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 public class Judgement {
     public int findSameNumbers(ArrayList<Integer> computer,ArrayList<Integer> player) {
         int sameCnt = 0;
-        for (Integer integer : player) {
-            if (computer.contains(integer)) {
+        for (int i = 0; i < player.size(); i++) {
+            if (computer.contains(player.get(i))) {
                 sameCnt++;
             }
         }

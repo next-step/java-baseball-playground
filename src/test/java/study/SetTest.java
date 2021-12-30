b66bd5c4e04bfd4271c67dc55924f4b448c7c557
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2021-12-30 16:04:36 +0900

    StringTest

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,32 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.HashSet;
+
+
+import java.util.Set;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    void test() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+}

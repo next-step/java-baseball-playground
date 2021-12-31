ec4a859eec31fa9487f225db1f7e37435a894c80
leesuengyeol <48343629+leesuengyeol@users.noreply.github.com>
2021-12-31 16:46:40 +0900

    ParameterizedTest

---
+++ src/test/java/Calculator/Calculator.java
@@ -0,0 +1,13 @@
+package Calculator;
+
+import java.util.Scanner;
+
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String value = scanner.nextLine();
+    }
+}

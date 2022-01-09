13a94542a1d8f499bb2aa90070dc04e11f78df43
Gunho Kang <dsds601@naver.com>
2022-01-09 21:39:50 +0900

    baseball :아웃 구현

--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -1,5 +1,6 @@
 import baseball2.domain.Judgment;
 import baseball2.domain.NumberGenerator;
+import baseball2.domain.Referee;
 
 import java.util.Arrays;
 import java.util.List;
@@ -7,14 +8,8 @@ import java.util.List;
 public class StringCalc {
 
     public static void main(String[] args) {
-//        final NumberGenerator numberGenerator = new NumberGenerator();
-//        final List<Integer> numbers = numberGenerator.createRandomMembers();
-//        System.out.println(numbers);
-
-        Judgment judgment = new Judgment();
-//        int count = judgment.correctCount(Arrays.asLiwst(4, 5, 6), Arrays.asList(1, 2, 3));
-//        System.out.println(count);
-        boolean result = judgment.hasPlace(Arrays.asList(7, 8, 9), 0,2);
+        Referee referee = new Referee();
+        String result = referee.compare(Arrays.asList(7, 8, 9), Arrays.asList(1, 2, 3));
         System.out.println(result);
     }
 }
--- src/main/java/baseball2/domain/Referee.java
+++ src/main/java/baseball2/domain/Referee.java
@@ -5,8 +5,22 @@ import java.util.List;
 public class Referee {
 
     public String compare(List<Integer> computer , List<Integer> player){
-
-        return "3 스트라이크";
+        // 몇개의 숫자가 같은지 알아낸 뒤
+        // 스트라이크의 개수를 구해 뺸다 -> 볼의 개수.
+        // 남은 수는 볼의 개수
+        Judgment judgment = new Judgment();
+        int correctCount = judgment.correctCount(computer, player);
+        int strike = 0;
+        for(int placeIndex = 0; placeIndex < player.size(); placeIndex++){
+            if(judgment.hasPlace(computer,placeIndex,player.get(placeIndex))) {
+                strike++;
+            }
+        }
+        int ball = correctCount - strike;
+        if( strike == 0){
+            return "아웃";
+        }
+        return ball + " 볼 " + strike +" 스트라이크";
     }
 
 

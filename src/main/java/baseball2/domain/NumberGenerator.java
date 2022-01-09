f3147441402616f2777b45888e7e1e95f3b8cc4d
Gunho Kang <dsds601@naver.com>
2022-01-09 20:57:20 +0900

    baseball : 중복되지않는 3개의 임의의수 생성 메서드 구현

--- src/main/java/baseball2/domain/NumberGenerator.java
+++ src/main/java/baseball2/domain/NumberGenerator.java
@@ -2,10 +2,22 @@ package baseball2.domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class NumberGenerator {
     public List<Integer> createRandomMembers(){
-
-        return  null;
+        //3개의 숫자가 담길 때까지
+        //만약 이미 존재하는 숫자라면 담지 않는다.
+        //만약 존재하지 않는 숫자라면 담는다.
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < 3){
+            int number = new Random().nextInt(9) + 1;
+            if(numbers.contains(number)){
+                continue;
+            } else {
+                numbers.add(number);
+            }
+        }
+        return  numbers;
     }
 }

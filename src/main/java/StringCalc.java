2cefd4f95d6b55f7a99a25a42dd6d3259f7f2cea
Gunho Kang <dsds601@naver.com>
2022-01-09 21:50:49 +0900

    baseball : 기능 완성

--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -2,14 +2,34 @@ import baseball2.domain.Judgment;
 import baseball2.domain.NumberGenerator;
 import baseball2.domain.Referee;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class StringCalc {
 
     public static void main(String[] args) {
+        NumberGenerator generator = new NumberGenerator();
+        List<Integer> computer = generator.createRandomMembers();
         Referee referee = new Referee();
-        String result = referee.compare(Arrays.asList(7, 8, 9), Arrays.asList(1, 2, 3));
-        System.out.println(result);
+        String result = "";
+        while (!result.equals("0 볼 3 스트라이크")) {
+            result = referee.compare(computer, askNumbers());
+            System.out.println(result);
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public static List<Integer> askNumbers(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("숫자를 입력해 주세요");
+        String input = scanner.nextLine();
+
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : input.split("")){
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 }
--- src/main/java/baseball2/domain/Referee.java
+++ src/main/java/baseball2/domain/Referee.java
@@ -17,7 +17,7 @@ public class Referee {
             }
         }
         int ball = correctCount - strike;
-        if( strike == 0){
+        if( correctCount == 0){
             return "아웃";
         }
         return ball + " 볼 " + strike +" 스트라이크";

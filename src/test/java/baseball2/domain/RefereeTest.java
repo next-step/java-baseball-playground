7deabc0dde0903c69e017adc98a9e89897b72884
Gunho Kang <dsds601@naver.com>
2022-01-09 22:08:22 +0900

    baseball : 테스트 코드 작성 완료

--- src/test/java/baseball2/domain/RefereeTest.java
+++ src/test/java/baseball2/domain/RefereeTest.java
@@ -3,6 +3,8 @@ package baseball2.domain;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 import java.util.List;
@@ -18,6 +20,14 @@ class RefereeTest {
     @BeforeEach
     void setUp() {
         referee = new Referee();
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1,2,3,0 볼 3 스트라이크","7,8,9,아웃","2,3,1,3 볼 0 스트라이크","2,1,3,2 볼 1 스트라이크"})
+    public void compare(int number1, int number2 , int number3 , String expected){
+        String actual = referee.compare(ANSWER,Arrays.asList(number1,number2,number3));
+        assertThat(actual).isEqualTo(expected);
+
     }
 
     @Test

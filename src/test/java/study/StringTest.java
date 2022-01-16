a21df143ae1e49773c297b3b01cf9e2949e3b48e
db.han <sudo.db.han@gmail.com>
2022-01-16 17:52:28 +0900

    refactor: 요구사항3 구현 리팩토링
    https://edu.nextstep.camp/s/RFY359FE/ls/p3fMT9lf

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -64,13 +64,12 @@ public class StringTest {
 		@ParameterizedTest
 		@CsvSource({"0,a", "1,b", "2,c"})
 		@DisplayName("\"abc\" 값이 주어졌을 때 String의 charAt() 메소드를 활용해 특정 위치의 문자를 가져오는 학습 테스트를 구현한다.")
-		public void charAt_test1(String givenIndexSource, String expectedString) {
+		public void charAt_test1(int givenIndex, char expected) {
 			String given = "abc";
 
-			int givenIndex = Integer.parseInt(givenIndexSource);
-            String actual = String.valueOf(given.charAt(givenIndex));
+            char actual = given.charAt(givenIndex);
 
-			assertEquals(expectedString, actual);
+			assertEquals(expected, actual);
 
 			assertThatThrownBy(() -> {
 				given.charAt(given.length());

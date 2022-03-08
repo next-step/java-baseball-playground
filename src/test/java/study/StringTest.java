658bcce49ec3b67a5db157ab6e3903e1a3ec0721
muttjimmypark <muttpark57@gmail.com>
2022-03-08 20:40:25 +0900

    studying testcase 1,2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,24 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split(){
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly("1","2");
+        actual = "1".split(",");
+        assertThat(actual).contains("1");
+    }
+
+    @Test
+    void substring(){
+        String actual = "(1,2)";
+        actual = actual.substring(actual.indexOf("(")+1, actual.indexOf(")")-1);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt(){
+        
+    }
 }

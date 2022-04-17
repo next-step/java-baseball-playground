22945b68eb672ac7a33142c43ac390ca81cc4897
taehyungz <thkthk97@naver.com>
2022-04-17 22:31:59 +0900

    feat: 3스트라이크가 나오면 게임을 종료하고 새로 시작할지 프로그램을 종료할 지를 묻는다

--- src/main/java/study/numberBaseballGame/NumberBaseballGame.java
+++ src/main/java/study/numberBaseballGame/NumberBaseballGame.java
@@ -6,26 +6,44 @@ public class NumberBaseballGame {
     private final ThreeNumbers trial = new ThreeNumbers();
     private final ThreeNumbers answer = new ThreeNumbers();
     private final NumberMatcher matcher = new NumberMatcher();
+    private boolean continueToken;
     Scanner sc = new Scanner(System.in);
     private Score score;
 
-    public NumberBaseballGame init() {
+    private void init() {
         trial.init();
         answer.init();
         answer.generateThreeRandomNumbers();
-        return this;
+        continueToken = true;
     }
 
     public void play() {
+        init();
         do {
-            System.out.println("숫자를 입력해주세요 : ");
+            continueToken = askAgainUntilWin();
+        } while (continueToken);
+    }
+
+    private boolean askAgainUntilWin() {
+        do {
             insertExpectedThreeNumbers(sc.nextLine());
             printScore();
-        } while(isWinningStatus());
+        } while (!isWinningStatus());
+
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n");
+        if (isGameContinueRequest(sc.nextInt())) return true;
+        return false;
+    }
+
+    private boolean isGameContinueRequest(int continueInput) {
+        if (continueInput != 1 && continueInput != 2) throw new IllegalArgumentException("올바르지 않은 숫자입니다.");
+        return continueInput == 1;
     }
 
     private void printScore() {
-        System.out.printf("%d볼 %d스트라이크\n", score.getStrike(), score.getBall());
+        System.out.printf("\n%d볼", score.getBall());
+        System.out.printf("%d스트라이크", score.getStrike());
+        if (isWinningStatus()) System.out.println("\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n");
     }
 
     private boolean isWinningStatus() {
@@ -33,6 +51,7 @@ public class NumberBaseballGame {
     }
 
     public void insertExpectedThreeNumbers(String input) {
+        System.out.println("숫자를 입력해주세요 : ");
         trial.insertExpectedThreeNumbers(input);
         score = matcher.calculateScore(trial, answer);
     }

7086a8650d0a1e1e101810a7a3797d9d9e1f91b4
taehyungz <thkthk97@naver.com>
2022-04-17 21:32:18 +0900

    feat: 입력 결과를 출력하고, 3스트라이크가 아니면 숫자를 다시 입력받는다

--- src/main/java/study/numberBaseballGame/NumberBaseballGame.java
+++ src/main/java/study/numberBaseballGame/NumberBaseballGame.java
@@ -1,21 +1,39 @@
 package study.numberBaseballGame;
 
+import java.util.Scanner;
+
 public class NumberBaseballGame {
     private final ThreeNumbers trial = new ThreeNumbers();
     private final ThreeNumbers answer = new ThreeNumbers();
     private final NumberMatcher matcher = new NumberMatcher();
+    Scanner sc = new Scanner(System.in);
     private Score score;
 
-    public NumberBaseballGame play() {
+    public NumberBaseballGame init() {
         trial.init();
         answer.init();
         answer.generateThreeRandomNumbers();
         return this;
     }
 
-    public NumberBaseballGame insertExpectedThreeNumbers(String input) {
+    public void play() {
+        do {
+            System.out.println("숫자를 입력해주세요 : ");
+            insertExpectedThreeNumbers(sc.nextLine());
+            printScore();
+        } while(isWinningStatus());
+    }
+
+    private void printScore() {
+        System.out.printf("%d볼 %d스트라이크\n", score.getStrike(), score.getBall());
+    }
+
+    private boolean isWinningStatus() {
+        return score.getStrike() == 3;
+    }
+
+    public void insertExpectedThreeNumbers(String input) {
         trial.insertExpectedThreeNumbers(input);
         score = matcher.calculateScore(trial, answer);
-        return this;
     }
 }

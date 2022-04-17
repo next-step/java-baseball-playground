db61ac56a1b5f435d638f4f0514eabe0719926d8
taehyungz <thkthk97@naver.com>
2022-04-17 20:46:25 +0900

    feat: 유니크한 숫자 세 개가 있는 문자열을 입력받는다.

--- src/main/java/study/numberBaseballGame/NumberBaseballGame.java
+++ src/main/java/study/numberBaseballGame/NumberBaseballGame.java
@@ -1,11 +1,16 @@
 package study.numberBaseballGame;
 
 public class NumberBaseballGame {
-
+    private ThreeNumbers trial;
     private ThreeNumbers answer;
 
     public NumberBaseballGame play() {
         answer.generateThreeRandomNumbers();
+        return this;
+    }
+
+    public NumberBaseballGame insertExpectedThreeNumbers(String input) {
+        trial.insertExpectedThreeNumbers(input);
         return this;
     }
 }
--- src/main/java/study/numberBaseballGame/ThreeNumbers.java
+++ src/main/java/study/numberBaseballGame/ThreeNumbers.java
@@ -8,6 +8,21 @@ public class ThreeNumbers {
     private int second;
     private int third;
 
+    public ThreeNumbers insertExpectedThreeNumbers(String s) {
+        assertInputString(s);
+        first = Integer.parseInt(s.substring(0, 1));
+        second = Integer.parseInt(s.substring(1, 2));
+        third = Integer.parseInt(s.substring(2,3));
+        return this;
+    }
+
+    private void assertInputString(String s) {
+        if (s == null || s.length() != 3) throw new IllegalArgumentException("입력된 문자열의 길이가 3이 아닙니다.");
+        if (!s.chars().allMatch(Character::isDigit)) throw new IllegalArgumentException("숫자가 아닌 문자가 포함되어 있습니다.");
+        if (s.charAt(0) == s.charAt(1) || s.charAt(1) == s.charAt(2) || s.charAt(0) == s.charAt(2))
+            throw new IllegalArgumentException("숫자 세 개는 서로 다른 숫자여야합니다..");
+    }
+
     public void generateThreeRandomNumbers() {
         first = generateRandomNumber();
         second = generateRandomNumberWithoutBannedNumber(List.of(first));

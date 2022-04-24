23b03601f64db3404a18310fdc259e1f90f0934a
Jihun Park <jihunpark.tech@gmail.com>
2022-04-24 10:44:43 +0900

    fix: test method name

--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -9,7 +9,7 @@ public class BallTest {
 
     @Test
     @DisplayName("같은 수가 같은 자리에 있으면 스트라이크")
-    void checkNothing() {
+    void strike() {
         Ball com = new Ball(1, 1);
         BallStatus ballStatus = com.play(new Ball(1, 1));
         assertThat(ballStatus).isEqualTo(BallStatus.STRIKE);

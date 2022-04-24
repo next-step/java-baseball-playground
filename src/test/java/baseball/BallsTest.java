cae26ccd0545e5b81fe0f1bbbe1e48ef8e569a46
Jihun Park <jihunpark.tech@gmail.com>
2022-04-24 11:24:21 +0900

    test: extended ball check
    컴퓨터가 선택한 3개의 숫자와 사용자가 입력한 하나의 공이 볼

--- src/test/java/baseball/BallsTest.java
+++ src/test/java/baseball/BallsTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -9,11 +10,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallsTest {
 
+    private Balls balls;
+
+    @BeforeEach
+    void setUp() {
+        balls = new Balls(Arrays.asList(1, 2, 3));
+    }
+
     @Test
     @DisplayName("컴퓨터가 선택한 3개의 숫자와 사용자가 입력한 하나의 공이 스트라이크")
     void strike() {
-        Balls balls = new Balls(Arrays.asList(1, 2, 3));
         BallStatus ballStatus = balls.play(new Ball(1, 1));
-        assertThat(ballStatus).isEqualTo(ballStatus);
+        assertThat(ballStatus).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @Test
+    @DisplayName("컴퓨터가 선택한 3개의 숫자와 사용자가 입력한 하나의 공이 볼")
+    void ball() {
+        BallStatus ballStatus = balls.play(new Ball(3, 1));
+        assertThat(ballStatus).isEqualTo(BallStatus.BALL);
     }
 }

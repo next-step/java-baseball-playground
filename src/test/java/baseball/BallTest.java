ee75c199001596f82a86f5ac5c6b3d02d8e4d24e
Jihun Park <jihunpark.tech@gmail.com>
2022-04-24 10:53:39 +0900

    refactor: add setup for computer ball

--- src/test/java/baseball/BallTest.java
+++ src/test/java/baseball/BallTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,10 +8,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class BallTest {
 
+    private Ball com;
+
+    @BeforeEach
+    void setUp() {
+        com = new Ball(1, 1);
+    }
+
     @Test
     @DisplayName("같은 수가 같은 자리에 있으면 스트라이크")
     void strike() {
-        Ball com = new Ball(1, 1);
         BallStatus ballStatus = com.play(new Ball(1, 1));
         assertThat(ballStatus).isEqualTo(BallStatus.STRIKE);
     }
@@ -18,7 +25,6 @@ public class BallTest {
     @Test
     @DisplayName("같은 숫자가 있지만 다른 자리에 있으면 볼")
     void ball() {
-        Ball com = new Ball(1, 1);
         BallStatus ballStatus = com.play(new Ball(3, 1));
         assertThat(ballStatus).isEqualTo(BallStatus.BALL);
     }
@@ -26,7 +32,6 @@ public class BallTest {
     @Test
     @DisplayName("같은 수가 아니면 낫싱")
     void nothing() {
-        Ball com = new Ball(1, 1);
         BallStatus ballStatus = com.play(new Ball(3, 2));
         assertThat(ballStatus).isEqualTo(BallStatus.NOTHING);
     }

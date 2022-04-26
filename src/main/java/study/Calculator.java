a194ce0f892a6e5f547d3b86ca194225c3676060
조지헌 <jojiapp@gmail.com>
2022-04-27 00:13:04 +0900

    [add] calculator

--- src/main/java/study/Calculator.java
+++ src/main/java/study/Calculator.java
@@ -1,27 +1,57 @@
 package study;
 
-import static java.lang.Integer.*;
-
 public class Calculator {
 
     public int calculateSeparateString(String str) {
         String[] strs = str.split(" ");
+        validationLength(strs.length);
+
         int result = parseInt(strs[0]);
 
         for (int i = 1; i < strs.length; i += 2) {
-            result = calculation(result, strs[i], parseInt(strs[i + 1]));
+            result = calculate(result, strs[i], parseInt(strs[i + 1]));
         }
         return result;
     }
 
+    private void validationLength(int length) {
+        if (length % 2 == 0) throw new IllegalArgumentException("공백을 기준으로 홀수여야 합니다.");
+    }
 
-    private int calculation(int result, String operation, int number) {
+    private int parseInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("짝수에는 숫자만 존재해야 합니다.");
+        }
+
+    }
+
+
+    private int calculate(int result, String operation, int number) {
         return switch (operation) {
-            case "+" -> result + number;
-            case "-" -> result - number;
-            case "*" -> result * number;
-            case "/" -> result / number;
+            case "+" -> add(result, number);
+            case "-" -> subtraction(result, number);
+            case "*" -> multiplication(result, number);
+            case "/" -> division(result, number);
             default -> throw new IllegalArgumentException("지원하지 안는 연산자 입니다.");
         };
+    }
+
+    private int division(int result, int number) {
+        if(number == 0) throw new IllegalStateException("0으로는 나눌수 없습니다.");
+        return result / number;
+    }
+
+    private int multiplication(int result, int number) {
+        return result * number;
+    }
+
+    private int subtraction(int result, int number) {
+        return result - number;
+    }
+
+    private int add(int result, int number) {
+        return result + number;
     }
 }
--- src/test/java/study/CalculatorTest.java
+++ src/test/java/study/CalculatorTest.java
@@ -30,11 +30,47 @@ public class CalculatorTest {
     @CsvSource({
             "1 + 2 + 3,6",
             "5 * 10 / 25,2",
-            "5,5"
+            "5,5",
+            "0 / 1,0"
     })
     void calculateSeparateString(String str, int result) throws Exception {
         int actual = calculator.calculateSeparateString(str);
         assertThat(actual).isEqualTo(result);
+    }
+
+    @DisplayName("짝수가 숫자가 아니면 IllegalArgumentException이 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"a", "0 + +", "10 + 5 - ="})
+    void even_only_number(String str) throws Exception {
+        assertThatThrownBy(() -> calculator.calculateSeparateString(str))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("짝수에는 숫자만 존재해야 합니다.");
+    }
+
+    @DisplayName("홀수가 사칙연산자가 아니면 IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"0 1 2", "1 + 2 3 3"})
+    void odd_only_separate(String str) throws Exception {
+        assertThatThrownBy(() -> calculator.calculateSeparateString(str))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("지원하지 안는 연산자 입니다.");
+    }
+
+    @DisplayName("배열 길이가 홀수가 아니거나 사이즈가 0이라면 ArrayIndexOutOfBoundsException예외가 발생한다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1 2", "1 + 2 3", " "})
+    void split_length_odd(String str) throws Exception {
+        assertThatThrownBy(() -> calculator.calculateSeparateString(str))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("공백을 기준으로 홀수여야 합니다.");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1 / 0", "0 + 5 / 0"})
+    void zero_by_division(String str) throws Exception {
+        assertThatThrownBy(() -> calculator.calculateSeparateString(str))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("0으로는 나눌수 없습니다.");
     }
 
 

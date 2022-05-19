4a37e470754f504f9e08c2bc92bf0cb89a13d505
Joonwoo Kim <kkn8010@gmail.com>
2022-05-19 22:24:45 +0900

    test: splitStringTest 코드 추가

--- src/main/java/stringcal/StringCalculator.java
+++ src/main/java/stringcal/StringCalculator.java
@@ -1,47 +1,38 @@
 package stringcal;
 
+import java.io.IOException;
+import java.util.Scanner;
+
 public class StringCalculator {
 
+    StringCalculator stringCalculator;
+    //더하기
     public int add(int i, int j){
         return i+j;
     }
 
+    //빼기
     public int subtract(int i, int j){
         return i-j;
     }
 
+    //곱하기
     public int multiply(int i, int j){
         return i*j;
     }
 
-    public int divide(int i, int j){
-        try{
-            return i/j;
-        }catch(Exception e){
-            System.out.println("0으로 나눌수 없습니다.");
+    //나누기(몫이 0일경우 Exception)
+    public int divide(int i, int j) {
+        try {
+            return i / j;
+        } catch (Exception e) {
+            System.out.println("0으로 나눌 수 없습니다.");
         }
         return 0;
     }
 
-    //결과값이 null이거나 공백일 경우
-    public static void notnull(Object k)throws IllegalAccessException{
-        if(k == null || k == ""){
-            throw new IllegalAccessException("공백이 있습니다.");
-        }
-    }
-
-    //사칙연산 부분을 각각의 메서드로 나누기
-    public int calculate(int firstvalue, char operator, int secondvalue){
-        if(operator == '+')
-            return add(firstvalue, secondvalue);
-        if(operator == '-')
-            return subtract(firstvalue, secondvalue);
-        if(operator == '*')
-            return multiply(firstvalue, secondvalue);
-        if(operator == '/')
-            return divide(firstvalue, secondvalue);
-        else
-            System.out.println("잘못된 값 입력");
-        throw new RuntimeException();
+    //문자열 자르는 메서드
+    public String[] splitString(String str){
+        return str.split("");
     }
 }

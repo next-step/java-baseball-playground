6e36b9c02241ed8423c26c3f6f3e342f15d0abbd
Kyujin Kang <digiry@gmail.com>
2022-06-22 07:55:08 +0900

    test(StringCalculator): update calculate() to perform calculating without operator orders

--- src/main/java/study/StringCalculator.java
+++ src/main/java/study/StringCalculator.java
@@ -1,23 +1,77 @@
 package study;
 
+import java.util.Scanner;
+
 public class StringCalculator {
 
-    public int calculate(int left, String operator, int right) {
+    private ScannerUtil scanner;
+
+    public interface ScannerUtil {
+        String nextLine();
+    }
+
+    public class CalculatorScanner implements ScannerUtil {
+
+        @Override
+        public String nextLine() {
+            return new Scanner(System.in).nextLine();
+        }
+    }
+
+    public StringCalculator() {
+        scanner = new CalculatorScanner();
+    }
+
+    public void setScanner(ScannerUtil scannerUtil) {
+        scanner = scannerUtil;
+    }
+
+    public String inputFormula() {
+        return scanner.nextLine();
+    }
+
+    public String[] splitFormula(String formula) {
+        return formula.split(" ");
+    }
+
+    public int calculateSimple(int left, String operator, int right) {
         int result = 0;
-        if (operator == "+") {
+        if ("+".equals(operator)) {
             result = left + right;
-        } else if (operator == "-") {
+        } else if ("-".equals(operator)) {
             result = left - right;
-        } else if (operator == "*") {
+        } else if ("*".equals(operator)) {
             result = left * right;
-        } else if (operator == "/") {
+        } else if ("/".equals(operator)) {
             if (right == 0) {
                 throw new IllegalArgumentException("Division by zero");
             }
             result = left / right;
         } else {
-            throw new IllegalArgumentException(operator + " not supported operator");
+            throw new IllegalArgumentException("Operator[" + operator + "] is not supported operator");
         }
         return result;
+    }
+
+    public int calculate(String[] units) {
+        int lhs = 0;
+        String operator = "";
+        int rhs = 0;
+        int index = 0;
+
+        lhs = Integer.parseInt(units[index]);
+        index++;
+
+        while (index < units.length) {
+            operator = units[index];
+            index++;
+
+            rhs = Integer.parseInt(units[index]);
+            index++;
+
+            lhs = calculateSimple(lhs, operator, rhs);
+        }
+
+        return lhs;
     }
 }
--- src/test/java/study/StringCalculatorTest.java
+++ src/test/java/study/StringCalculatorTest.java
@@ -6,6 +6,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import study.StringCalculator.ScannerUtil;
 
 public class StringCalculatorTest {
     private StringCalculator calc;
@@ -19,7 +20,7 @@ public class StringCalculatorTest {
     @DisplayName("간단한 이항식 문자열을 정상적으로 계산하는지 확인한다")
     void calculateSimpleFormula() {
         int expected = 5;
-        int actual = calc.calculate(2, "+", 3);
+        int actual = calc.calculateSimple(2, "+", 3);
         assertThat(actual).isEqualTo(expected);
     }
 
@@ -27,7 +28,7 @@ public class StringCalculatorTest {
     @DisplayName("지원하지 않는 연산자를 전달할 때 예외를 발생하는지 확인한다")
     void throwExceptionWhenNotSupportedOperatorPassed() {
         assertThatThrownBy(() -> {
-            int actual = calc.calculate(2, "!", 3);
+            int actual = calc.calculateSimple(2, "!", 3);
         }
         ).isInstanceOf(IllegalArgumentException.class);
     }
@@ -36,7 +37,67 @@ public class StringCalculatorTest {
     @DisplayName("0으로 나누는 값을 전달할 때 예외를 발생하는지 확인한다")
     void throwExceptionWhenDividedByZero() {
         assertThatThrownBy(() -> {
-            int actual = calc.calculate(2, "/", 0);
+            int actual = calc.calculateSimple(2, "/", 0);
         }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    class FakeScanner implements ScannerUtil {
+
+        String value;
+        FakeScanner(String input) {
+            value = input;
+        }
+        @Override
+        public String nextLine() {
+            return value;
+        }
+    }
+
+    void makeStringCalculatorWithFakeScanner(String userInput) {
+        FakeScanner fakeScanner = new FakeScanner(userInput);
+        calc.setScanner(fakeScanner);
+    }
+
+    @Test
+    @DisplayName("사용자로부터 계산식을 입력받기를 확인한다")
+    void inputFormula() {
+        String expected = "2 + 3 * 5 / 3";
+        makeStringCalculatorWithFakeScanner(expected);
+
+        String actual = calc.inputFormula();
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("문자열 계산식을 빈칸으로 쪼갠 계산 배열을 확인한다")
+    void splitFormula() {
+        String expected = "2 + 3 * 5 / 3";
+        makeStringCalculatorWithFakeScanner(expected);
+        String formula = calc.inputFormula();
+
+        String[] actualUnits = calc.splitFormula(formula);
+
+        assertThat(actualUnits).contains("2");
+        assertThat(actualUnits).contains("+");
+        assertThat(actualUnits).contains("3");
+        assertThat(actualUnits).contains("*");
+        assertThat(actualUnits).contains("5");
+        assertThat(actualUnits).contains("/");
+        assertThat(actualUnits).contains("3");
+    }
+
+    @Test
+    @DisplayName("문자열 계산식을 연산자 우선순위 무사한 순서대로 계산한 결과를 확인한다")
+    void calculateFormulaWithIgnoringOperatorOrder() {
+        String fakeInput = "2 + 3 * 5 / 3";
+        int expected = 8;
+        makeStringCalculatorWithFakeScanner(fakeInput);
+        String formula = calc.inputFormula();
+        String[] actualUnits = calc.splitFormula(formula);
+
+        int actual = calc.calculate(actualUnits);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }

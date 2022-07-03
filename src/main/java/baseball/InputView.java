4e3144682a987142264f2bed0565f78a0b5b8506
Kyujin Kang <digiry@gmail.com>
2022-07-03 15:18:44 +0900

    feat(InputView): read user input and verify them

---
+++ src/main/java/baseball/InputView.java
@@ -0,0 +1,51 @@
+package baseball;
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class InputView {
+
+    ScannerUtil scannerUtil;
+    final String TRIPLE_DIGIT_REGEX_PATTERN = "^\\d{3}$";
+
+    public interface ScannerUtil {
+        String nextLine();
+    }
+
+    static class JavaScanner implements ScannerUtil {
+
+        @Override
+        public String nextLine() {
+            Scanner scanner = new Scanner(System.in);
+            return scanner.nextLine();
+        }
+    }
+
+    public InputView(ScannerUtil util) {
+        scannerUtil = util;
+    }
+
+    public InputView() {
+        scannerUtil = new JavaScanner();
+    }
+
+    public Boolean verifyUniqueTripleGameDigits(String userInput) {
+        if (userInput.charAt(0) == userInput.charAt(1)) {
+            return false;
+        }
+
+        if (userInput.charAt(0) == userInput.charAt(2)) {
+            return false;
+        }
+
+        return userInput.charAt(1) != userInput.charAt(2);
+    }
+
+    public Boolean verifyTripleGameDigits(String userInput) {
+        return Pattern.matches(TRIPLE_DIGIT_REGEX_PATTERN, userInput);
+    }
+
+    public void printInputTripleGameDigitsMessage() {
+        System.out.print("숫자를 입력해 주세요 : ");
+    }
+}
---
+++ src/test/java/baseball/InputViewTest.java
@@ -0,0 +1,47 @@
+package baseball;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import baseball.InputView.ScannerUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class InputViewTest {
+
+    static class FakeScanner implements ScannerUtil {
+
+        String buffer;
+
+        public FakeScanner(String input) {
+            buffer = input;
+        }
+
+        @Override
+        public String nextLine() {
+            return buffer;
+        }
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:true", "12a:false", "1234:false"}, delimiter = ':')
+    @DisplayName("3자리 야구 숫자 문자열을 검사한다")
+    void verifyTripleGameDigits(String input, Boolean expected) {
+        InputView inputView = new InputView();
+
+        Boolean actual = inputView.verifyTripleGameDigits(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:true", "112:false"}, delimiter = ':')
+    @DisplayName("중복 없는 3자리 야구 숫자열을 검사한다")
+    void verifyUniqueTripleGameDigits(String input, Boolean expected) {
+        InputView inputView = new InputView();
+
+        Boolean actual = inputView.verifyUniqueTripleGameDigits(input);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}

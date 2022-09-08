74e150a5bc167e354e6b7428ad89b7c869bb3cd6
joomin <joominl15@gmail.com>
2022-09-08 21:20:35 +0900

    request 2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -13,7 +13,7 @@ public class StringTest {
 
     @Test
     void request1(){
-        /**
+        /*
          - "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 학습 테스트를 구현한다.
          - "1"을 ,로 split 했을 때 1만을 포함하는 배열이 반환되는지에 대한 학습 테스트를 구현한다.
 
@@ -34,4 +34,17 @@ public class StringTest {
     }
 
 
+    @Test
+    void request2(){
+        /*
+        - "(1,2)" 값이 주어졌을 때 String의 substring() 메소드를 활용해 ()을 제거하고 "1,2"를 반환하도록 구현한다.
+         **/
+        String nums = "(1,2)";
+        int delIndex1 = nums.indexOf("(") +1; //index가 0이므로 +1 인덱스값부터 필요한 문자열
+        int delIndex2 = nums.indexOf(")");
+
+        String finNum = nums.substring(delIndex1,delIndex2);
+
+        assertThat(finNum).isEqualTo("1,2");
+    }
 }

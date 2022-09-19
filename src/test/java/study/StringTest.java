70aa451661334bc6ced3913f1f9028a003f16769
Rayeyoo <yuhw9324@naver.com>
2022-09-20 07:24:14 +0900

    "1,2"을 ,로 split 했을 때 1과 2로 잘 분리되는지 확인하는 테스트 구현.

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,16 @@
 package study;
 
 import org.junit.jupiter.api.Test;
-
 import static org.assertj.core.api.Assertions.assertThat;
-
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+    @Test
+    void split(){
+        String splitVar = "12";
+        assertThat(splitVar).contains("1", "2");
     }
 }

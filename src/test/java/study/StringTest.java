ef106f4afd58915c73e8a7b11334b7e15ca711c4
Adios <30585897+jongwanS@users.noreply.github.com>
2022-09-21 00:31:43 +0900

    req1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,18 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    @DisplayName("요구사항1")
+    public void req1(){
+        String str = "1,2";
+        String[] strSplit = str.split(",");
+        assertThat(strSplit).contains("1", "2");
+
+        str = "1";
+        strSplit = str.split(",");
+
+        assertThat(strSplit).containsExactly("1");
     }
 }

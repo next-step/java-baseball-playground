641e5fc3cb9b6d84a614b731644eed7a2e415d6d
syndersonLEE <winnery93@gmail.com>
2022-09-26 00:11:34 +0900

    update StringTest & SetTest

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,38 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("Valid Set Size")
+    public void testSize() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("Valid Set Contain without Duplicated")
+    public void contains(int number) {
+
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,53 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.ArrayList;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    public void split() {
+        String[] values = "1".split(",");
+        assertThat(values).contains("1");
+        values = "1,2".split(",");
+        assertThat(values).containsExactly("1","2");
+    }
+
+    @Test
+    public void substring() {
+        String input = "{1,2}";
+        String result = input.substring(1, input.length() - 1);
+        assertThat(result).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("문자열 위치 가져오기")
+    public void charAt() {
+        String input = "abc";
+        int idx = 5;
+        assertThatThrownBy(() -> {
+            input.charAt(idx);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("Index : %d, Size : %d", idx, input.length());
+    }
+
+    @Test
+    @DisplayName("문자열 위치 가져오기2")
+    public void charAt2() {
+        String input = "abc";
+        int idx = 5;
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> {
+                    input.charAt(idx);
+                }).withMessageMatching("Index: \\d+, Size: \\d+");
     }
 }

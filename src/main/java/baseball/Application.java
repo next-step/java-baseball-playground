5bb51b97b7702af7311de55120d6fa33fbb7cfdc
khan <>
2022-09-30 01:15:51 +0900

    feat: main 메소드 구현

---
+++ src/main/java/baseball/Application.java
@@ -0,0 +1,13 @@
+package baseball;
+
+//import camp.nextstep.edu.missionutils.Console;
+
+
+import baseball.domain.Referee;
+
+public class Application {
+    public static void main(String[] args) {
+        Referee referee = new Referee();
+        referee.startGame();
+    }
+}

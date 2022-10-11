9ec904138be04fd4d9f00f0b0b6197ccb078846c
전소연 <jeonsoyeon@jeonsoyeon-ui-MacBook.local>
2022-10-11 15:52:19 +0900

    feat: 구현완료

--- src/main/java/BaseballMain.java
+++ src/main/java/BaseballMain.java
@@ -1,11 +1,33 @@
 import domain.NumberGenerator;
+import domain.Referee;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BaseballMain {
     public static void main(String[] args) {
         NumberGenerator generator = new NumberGenerator();
-        List<Integer> numbers = generator.CreateRandomNumbers();
-        System.out.println(numbers);
+        List<Integer> computer = generator.CreateRandomNumbers();
+
+        Referee referee = new Referee();
+        String result ="";
+        while(!result.equals("0볼 3스트라이크")){
+            result = referee.compare(computer, askNumbers());
+        }
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료!");
+    }
+
+    public static List<Integer> askNumbers() {
+
+        System.out.println("숫자를 입력해 주세요 : ");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        List<Integer> numbers = new ArrayList<>();
+        for(String number : input.split("")){
+            numbers.add(Integer.valueOf(number));
+        }
+        return numbers;
     }
 }
--- src/main/java/domain/Referee.java
+++ src/main/java/domain/Referee.java
@@ -3,20 +3,20 @@ package domain;
 import java.util.List;
 
 public class Referee {
-    public String compare(List<Integer> computer, List<Integer> player ){
+    public String compare(List<Integer> computer, List<Integer> player) {
         Judgment judgment = new Judgment();
         int correctCount = judgment.correctCount(computer, player);
-        int strike =0;
-        for(int placeIndex = 0; placeIndex<player.size(); placeIndex++){
-            if(judgment.hasPlace(computer,placeIndex,player.get(placeIndex))){
+        int strike = 0;
+        for (int placeIndex = 0; placeIndex < player.size(); placeIndex++) {
+            if (judgment.hasPlace(computer, placeIndex, player.get(placeIndex))) {
                 strike++;
             }
         }
-        int ball = correctCount-strike;
+        int ball = correctCount - strike;
 
-        if(correctCount==0){
+        if (correctCount == 0) {
             return "낫싱";
         }
-        return ball +"볼"+strike+"스트라이크";
+        return ball + "볼 " + strike + "스트라이크";
     }
 }

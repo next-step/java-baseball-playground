8a90fc8b7404bd5939fe35b1c22e528ab38fe86d
전소연 <jeonsoyeon@jeonsoyeon-ui-MacBook.local>
2022-10-11 14:23:05 +0900

    몇 개의 수가 같은지 알 수 있는 기능 구현

--- src/main/java/domain/judgment.java
+++ src/main/java/domain/judgment.java
@@ -1,4 +1,20 @@
-package baseball.domain;
+package domain;
+
+import java.util.List;
 
 public class judgment {
+    public int correctCount(List<Integer> computer, List<Integer> player) {
+        int result = 0;
+        for (int i = 0; i < player.size(); i++) {
+            int playerNumber = player.get(i);
+            if (computer.contains(playerNumber)) {
+                result++;
+            }
+        }
+        return 0;
+    }
+
+    public boolean hasPlace(int placeNumber, int number) {
+        return true;
+    }
 }

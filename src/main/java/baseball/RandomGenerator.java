f262dfccb614aaf6ffc9bb508d40b71717499f06
sghwan <anan123456@naver.com>
2022-10-21 16:11:49 +0900

    feat(RandomGenerator): create createRandomNumbers method that pick number which is not duplicate

---
+++ src/main/java/baseball/RandomGenerator.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RandomGenerator {
+    private static final int DIGIT_COUNT = 3;
+
+    public List<Integer> createRandomNumbers() {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
+        List<Integer> numbers = new ArrayList<>();
+
+        for (int i = 0; i < DIGIT_COUNT; i++) {
+            int index = (int) (Math.random() * 10) % list.size();
+            numbers.add(list.remove(index));
+        }
+
+        return numbers;
+    }
+}
---
+++ src/test/java/baseball/RandomGeneratorTest.java
@@ -0,0 +1,20 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class RandomGeneratorTest {
+
+    RandomGenerator randomGenerator = new RandomGenerator();
+
+    @Test
+    @DisplayName("중복이 없는 3자리 숫자를 리턴한다.")
+    void createRandomNumbers() {
+        List<Integer> actual = randomGenerator.createRandomNumbers();
+        assertThat(actual.size()).isEqualTo(3);
+    }
+}

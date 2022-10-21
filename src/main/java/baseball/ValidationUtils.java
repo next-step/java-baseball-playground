5d0e172406a6b906dc8e95a12f22c2d15fe986a0
sghwan <anan123456@naver.com>
2022-10-21 15:43:18 +0900

    feat(ValidationUtils): create validateDigit method that check correct digit

--- src/main/java/baseball/ValidationUtils.java
+++ src/main/java/baseball/ValidationUtils.java
@@ -1,10 +1,17 @@
 package baseball;
 
+import java.util.List;
+
 public class ValidationUtils {
     private static final int MIN_VALUE = 1;
     private static final int MAX_VALUE = 9;
+    private static final int DIGIT_COUNT = 3;
 
     public boolean validateNumber(int number) {
         return number >= MIN_VALUE && number <= MAX_VALUE;
+    }
+
+    public boolean validateDigit(List<Integer> numbers) {
+        return numbers.size() == DIGIT_COUNT;
     }
 }
--- src/test/java/baseball/ValidationUtilsTest.java
+++ src/test/java/baseball/ValidationUtilsTest.java
@@ -1,10 +1,13 @@
 package baseball;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.Arrays;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -21,5 +24,17 @@ public class ValidationUtilsTest {
     @DisplayName("숫자값은 1 ~ 9의 값이다.")
     void validateNumberRange(int number, boolean expected) {
         assertThat(validationUtils.validateNumber(number)).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("게임을 하기 위한 숫자의 자리수는 세자리 이다.")
+    void validateThreeDigit() {
+        assertThat(validationUtils.validateDigit(Arrays.asList(1, 2, 3))).isTrue();
+        assertThat(validationUtils.validateDigit(Arrays.asList(1, 2))).isFalse();
+    }
+
+    @AfterEach
+    void tearDown() {
+        validationUtils = null;
     }
 }

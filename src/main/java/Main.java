f6f56a85bf411321887e15b3183fb0a1775f148a
s-ryuri <fbfbf1@naver.com>
2022-11-14 21:53:48 +0900

    Refactor : userInput 입력 함수 추출

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,50 +4,48 @@ import java.util.Scanner;
 
 public class Main {
 
-
-
     public static void main(String[] args) {
 
-        while(true){
+        while (true) {
 
             String answer = getAnswer();
 
-            while (true){
-                Scanner in = new Scanner(System.in);
+            while (true) {
+                String number = input();
 
-                System.out.print("숫자를 입력해 주세요 : ");
-                String number = in.nextLine();
-
-                if(number.equals(answer)){
+                if (number.equals(answer)) {
                     System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
                     System.out.println("정답은 : " + answer);
                     System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+
+                    Scanner in = new Scanner(System.in);
+
                     int num = in.nextInt();
-                    if(num == 1){
+                    if (num == 1) {
                         break;
-                    }else{
+                    } else {
                         return;
                     }
                 }
+
                 int ball = 0;
                 int strike = 0;
-                for(int i = 0;i<3;i++){
-                    for(int j = 0;j<3;j++){
-                        if(answer.charAt(i) == number.charAt(j)){
-                            if(i == j){
+                for (int i = 0; i < 3; i++) {
+                    for (int j = 0; j < 3; j++) {
+                        if (answer.charAt(i) == number.charAt(j)) {
+                            if (i == j) {
                                 strike++;
                                 break;
-                            }
-                            else{
+                            } else {
                                 ball++;
                             }
                         }
                     }
                 }
-                if(ball + strike == 0){
+                if (ball + strike == 0) {
                     System.out.println("Four Ball or Nothing");
                 }
-                System.out.println(ball +" 볼 " + strike +"스트라이크");
+                System.out.println(ball + " 볼 " + strike + "스트라이크");
             }
         }
     }
@@ -58,13 +56,13 @@ public class Main {
 
         String answer = "";
 
-        while(true){
+        while (true) {
             final HashSet<Integer> three = new HashSet<>();
-            for(int i = 0;i<3;i++){
+            for (int i = 0; i < 3; i++) {
                 final int randomNumber = rand.nextInt(9) + 1;
                 three.add(randomNumber);
             }
-            if(three.size() == 3){
+            if (three.size() == 3) {
                 answer = makeAnswer(three);
                 break;
             }
@@ -75,10 +73,18 @@ public class Main {
     private static String makeAnswer(final HashSet<Integer> three) {
         String answer = "";
 
-        for(Integer num : three){
+        for (Integer num : three) {
             answer += num;
         }
 
         return answer;
+    }
+
+    private static String input() {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("숫자를 입력해 주세요 : ");
+
+        return in.nextLine();
     }
 }

2a82b1811da38b5e68837471aa99033adb1498aa
s-ryuri <fbfbf1@naver.com>
2022-11-14 16:53:29 +0900

    feat : Main에 숫자야구게임 구현

---
+++ src/main/java/Main.java
@@ -0,0 +1,72 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+
+        while(true){
+            String answer = "";
+
+            while(true){
+                final HashSet<Integer> three = new HashSet<>();
+                for(int i = 0;i<3;i++){
+                    final int randomNumber = rand.nextInt(9) + 1;
+                    three.add(randomNumber);
+                }
+                if(three.size() == 3){
+                    for(Integer num : three){
+                        answer += num;
+                    }
+                    break;
+                }
+            }
+
+
+            while (true){
+                Scanner in = new Scanner(System.in);
+
+                System.out.print("숫자를 입력해 주세요 : ");
+                String number = in.nextLine();
+
+                if(number.equals(answer)){
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    System.out.println("정답은 : " + answer);
+                    System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요");
+                    int num = in.nextInt();
+                    if(num == 1){
+                        break;
+                    }else{
+                        return;
+                    }
+                }else{
+                    int ball = 0;
+                    int strike = 0;
+                    for(int i = 0;i<3;i++){
+                        for(int j = 0;j<3;j++){
+                            if(answer.charAt(i) == number.charAt(j)){
+                                if(i == j){
+                                    strike++;
+                                    break;
+                                }
+                                else{
+                                    ball++;
+                                }
+                            }
+                        }
+                    }
+                    if(ball + strike == 0){
+                        System.out.println("Four Ball or Nothing");
+                    }else{
+                        System.out.println(ball +" 볼 " + strike +"스트라이크");
+                    }
+
+                }
+            }
+        }
+    }
+}
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,7 +1,17 @@
 package study;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
@@ -10,4 +20,88 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    @DisplayName("1,2를 ,로 split 했을 때 [1,2]로 나오는 지 확인")
+    void test1(){
+        final String sentence = "1,2";
+        final String[] split = sentence.split(",");
+        assertThat(split).contains("1", "2");
+    }
+
+    @Test
+    @DisplayName("1을 ,로 split 했을 때 1만 포함하는 배열인지 확인")
+    void test2(){
+        String one = "1";
+        final String[] split = one.split(",");
+
+        assertThat(split).containsExactly("1");
+    }
+
+    @Test
+    @DisplayName("subString Test")
+    void test3(){
+        final String s = "(1,2)";
+        final String substring = s.substring(1, 4);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
+    @Test
+    void test4(){
+        final String abc = "abc";
+        assertThatThrownBy(() -> abc.charAt(5))
+            .isInstanceOf(IndexOutOfBoundsException.class);
+    }
+
+    @Test
+    void test5(){
+        final Set<Integer> number = new HashSet<>();
+        number.add(1);
+        number.add(1);
+        number.add(2);
+        number.add(3);
+
+        assertThat(number.size()).isEqualTo(3);
+    }
+
+    @Test
+    void test6(){
+        final Set<Integer> number = new HashSet<>();
+        number.add(1);
+        number.add(1);
+        number.add(2);
+        number.add(3);
+
+        assertThat(number.contains(1)).isTrue();
+        assertThat(number.contains(2)).isTrue();
+        assertThat(number.contains(3)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true","2:true","4:false"},delimiter = ':')
+    void test7(String input, String expected){
+        final Set<Integer> number = new HashSet<>();
+        number.add(1);
+        number.add(1);
+        number.add(2);
+        number.add(3);
+
+        Integer newInput = Integer.valueOf(input);
+        Boolean expect = Boolean.valueOf(expected);
+
+        assertThat(number.contains(newInput)).isEqualTo(expect);
+    }
+
+    @Test
+    @DisplayName("계산기")
+    void test8(){
+        Scanner in = new Scanner(System.in);
+        final String s = in.nextLine();
+        System.out.println(s);
+    }
+
+
+
+
+
 }

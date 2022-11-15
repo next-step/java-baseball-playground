d82a75f5e2a93f6b07598d2360aebb129964b0ba
YooDongSeon <inko51366@naver.com>
2022-11-15 21:34:22 +0900

    contains를 활용해 배열에 원하는 값이 포함되어있는지 확인

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,12 +2,20 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void splitTest_1() {
+        String[] actual = "1,2".split(",");
+
+        assertThat(actual).contains("1");
+        assertThat(actual).contains("2");
     }
 }

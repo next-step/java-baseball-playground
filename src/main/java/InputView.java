5b6b8baaf48910fb73df7ec77f9ac21f1ec4c4c6
s-ryuri <fbfbf1@naver.com>
2022-11-15 21:17:53 +0900

    Refactor : List<String> 전역 변수로

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -7,6 +7,8 @@ public class InputView {
     private Scanner scanner;
     private ResultView resultView;
 
+    private List<String> result = new ArrayList<>();
+
     public InputView() {
         scanner = new Scanner(System.in);
         resultView = new ResultView();
@@ -15,7 +17,6 @@ public class InputView {
 
     public List<String> input() {
         resultView.inputNumbers();
-        List<String> result = new ArrayList<>();
 
         while (true) {
             final String s = scanner.nextLine();
@@ -24,12 +25,16 @@ public class InputView {
                 continue;
             }
 
-            for (int i = 0; i < 3; i++) {
-                result.add(String.valueOf(s.charAt(i)));
-            }
+            getInputs(s);
             break;
         }
 
         return result;
+    }
+
+    private void getInputs(final String s) {
+        for (int i = 0; i < 3; i++) {
+            result.add(String.valueOf(s.charAt(i)));
+        }
     }
 }

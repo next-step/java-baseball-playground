0136644422e8431ec5f5a446ef0543bc9fb321d5
YooDongSeon <inko51366@naver.com>
2022-11-16 18:38:29 +0900

    요구사항 3 - 입력 값에 따라 결과 값이 다른 경우에 대한 테스트

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -3,6 +3,7 @@ package study;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.HashSet;
@@ -39,5 +40,12 @@ public class SetTest {
     @ValueSource(ints = {1, 2, 3})
     void deDuplicationContainsTest(int param) {
         assertTrue(numbers.contains(param));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void isNumberTrueOrFalse(int input, boolean bool) {
+
+        assertThat(numbers.contains(input)).isEqualTo(bool);
     }
 }

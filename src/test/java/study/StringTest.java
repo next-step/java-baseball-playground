3874c173b3a1b1a6e6bd38b4288fc5a00a7149a0
YooDongSeon <inko51366@naver.com>
2022-11-16 09:29:56 +0900

    Test Commit

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+    
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");

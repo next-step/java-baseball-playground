da0806b6af703a9b6853549cdd072b743e2447d4
YooDongSeon <inko51366@naver.com>
2022-11-17 19:46:59 +0900

    문자열을 계산하는 메서드 구현

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,13 +1,3 @@
-/**
- * 기능 정리 :
- *  1. nextLine()을 사용해 공백을 포함하여 입력받는다.
- *  2. split()을 사용해 공백을 기준으로 문자열들을 분리한다.
- *  3. 분리된 String 문자열들을 Integer.parseInt()을 사용해 형변환 해준다.
- *  4. 들어온 문자를 순서대로 계산해준다.
- *  5. 공백인지 아닌지 검사.
- *  6. 결과 반환.
- */
-
 public class StringCalculator {
     public String[] split(String s) {
         return s.split(" ");
@@ -17,23 +7,22 @@ public class StringCalculator {
         return Integer.parseInt(s);
     }
 
-    public int add(int a, int b) {
+    private int add(int a, int b) {
         return a + b;
     }
 
-    public int sub(int a, int b) {
+    private int sub(int a, int b) {
         return a - b;
     }
 
-    public int mul(int a, int b) {
+    private int mul(int a, int b) {
         return a * b;
     }
 
-    public int divide(int a, int b) {
+    private int divide(int a, int b) {
         try {
             return a / b;
-        }
-        catch (ArithmeticException e) {
+        } catch (ArithmeticException e) {
             System.out.println("0으로 나눌수 없슴미다");
         }
 
@@ -42,19 +31,29 @@ public class StringCalculator {
 
     public int calculate(int a, char c, int b) {
 
-        if(c == '+')
-            return add(a , b);
+        if (c == '+')
+            return add(a, b);
 
-        else if(c == '-')
+        else if (c == '-')
             return sub(a, b);
 
-        else if(c == '*')
-            return  mul(a, b);
+        else if (c == '*')
+            return mul(a, b);
 
-        else if(c == '/')
+        else if (c == '/')
             return divide(a, b);
 
         else
             throw new IllegalArgumentException("잘못된 값을 입력하셨습니다.");
+    }
+
+    public int calculateString(String[] strs) {
+        int result = toInt(strs[0]);
+
+        for (int i = 0; i < strs.length - 2; i += 2) {
+            result = calculate(result, strs[i + 1].charAt(0), toInt(strs[i + 2]));
+        }
+
+        return result;
     }
 }

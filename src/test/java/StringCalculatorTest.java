74b5be6db3c8ad2aa7fc5b087d607a7686c1762f
YooDongSeon <inko51366@naver.com>
2022-11-17 19:48:06 +0900

    code format

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -8,12 +8,12 @@ import static org.assertj.core.api.Assertions.*;
 
 /**
  * 기능 정리 :
- *  1. nextLine()을 사용해 공백을 포함하여 입력받는다.
- *  2. split()을 사용해 공백을 기준으로 문자열들을 분리한다.
- *  3. 분리된 String 문자열들을 Integer.parseInt()을 사용해 형변환 해준다.
- *  4. 들어온 문자를 순서대로 계산해준다.
- *  5. 공백인지 아닌지 검사.
- *  6. 결과 반환.
+ * 1. nextLine()을 사용해 공백을 포함하여 입력받는다.
+ * 2. split()을 사용해 공백을 기준으로 문자열들을 분리한다.
+ * 3. 분리된 String 문자열들을 Integer.parseInt()을 사용해 형변환 해준다.
+ * 4. 들어온 문자를 순서대로 계산해준다.
+ * 5. 공백인지 아닌지 검사.
+ * 6. 결과 반환.
  */
 
 class StringCalculatorTest {
@@ -59,7 +59,7 @@ class StringCalculatorTest {
     @Test
     @DisplayName("문자열을 계산")
     public void calculateStringTest() {
-        String[] strs = {"2", "+", "3",  "*", "4", "/", "2"};
+        String[] strs = {"2", "+", "3", "*", "4", "/", "2"};
         int result = cal.calculateString(strs);
 
         assertThat(result).isEqualTo(10);

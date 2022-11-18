3796cacf3459410df2de7d380e6638b1dd4b9162
YooDongSeon <inko51366@naver.com>
2022-11-18 22:01:01 +0900

    3자리 난수를 생성하는 메서드

--- src/main/java/numberBaseballGame/generateRandomNumber.java
+++ src/main/java/numberBaseballGame/generateRandomNumber.java
@@ -1,5 +1,22 @@
 package numberBaseballGame;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 public class generateRandomNumber {
 
+    public List<Integer> createRandomNumber() {
+        Set<Integer> randomSet = new HashSet();
+
+        while(randomSet.size() < 3) {
+            Double d = Math.random() * 10 + 1;
+            randomSet.add(d.intValue());
+        }
+
+        List<Integer> randomNumList = new ArrayList<>(randomSet);
+
+        return randomNumList;
+    }
 }

7cd52564f6858099d52ce0412e5125dfbb612dec
YooDongSeon <inko51366@naver.com>
2022-11-18 21:31:56 +0900

    list 추가

--- src/test/java/numberBaseballGame/generateRandomNumberTest.java
+++ src/test/java/numberBaseballGame/generateRandomNumberTest.java
@@ -4,19 +4,24 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 class generateRandomNumberTest {
 
     generateRandomNumber randomNumber;
+    List<Integer> list;
 
     @BeforeEach
     public void setRamdomNumber() {
         randomNumber = new generateRandomNumber();
+        list = new ArrayList<>();
     }
 
     @Test
     @DisplayName("1부터 10까지 난수가 생성되는지 테스트")
     public void randomNumberTest() {
 
-
+        randomNumber.createRandomNumber();
     }
 }

9d18a3c93a080a0ff210434188118b8d469968d7
YooDongSeon <inko51366@naver.com>
2022-11-19 16:20:59 +0900

    createRandomNumber Test

--- src/test/java/numberBaseballGame/generateRandomNumberTest.java
+++ src/test/java/numberBaseballGame/generateRandomNumberTest.java
@@ -4,27 +4,23 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class generateRandomNumberTest {
 
     generateRandomNumber randomNumber;
-    List<Integer> list;
 
     @BeforeEach
     public void setRamdomNumber() {
         randomNumber = new generateRandomNumber();
-        list = new ArrayList<>();
     }
 
     @Test
     @DisplayName("3자리 난수가 생성되는지 테스트")
     public void randomNumberTest() {
-        list = randomNumber.createRandomNumber();
+        String random = String.valueOf(randomNumber.createRandomNumber());
 
-        assertThat(list.size()).isEqualTo(3);
+        assertThat(random.length()).isEqualTo(3);
+
     }
 }

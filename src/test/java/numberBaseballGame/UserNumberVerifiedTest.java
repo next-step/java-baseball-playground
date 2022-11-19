87761ae697ffffe432035eac8ef35f4fa929862d
YooDongSeon <inko51366@naver.com>
2022-11-19 15:34:04 +0900

    입력받은 숫자가 유효한지 검증하는 테스트

--- src/test/java/numberBaseballGame/UserNumberVerifiedTest.java
+++ src/test/java/numberBaseballGame/UserNumberVerifiedTest.java
@@ -1,4 +1,26 @@
-import static org.junit.jupiter.api.Assertions.*;
+package numberBaseballGame;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
 class UserNumberVerifiedTest {
-  
+
+    UserNumberVerified numberVerified;
+    List<Integer> inputList;
+    
+    @ParameterizedTest
+    @DisplayName("입력받은 숫자가 유효한지 검증하는 메서드 테스트")
+    @ValueSource(strings = {"1234", " ", "1m", "112"})
+    public void verifiedInputNumber(String param) {
+
+        assertThatThrownBy(() -> new UserNumberVerified(param))
+                .isInstanceOf(IllegalArgumentException.class);
+
+    }
 }

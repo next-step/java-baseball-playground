699d14069f7c0241742483de34493ceef1f08295
YooDongSeon <inko51366@naver.com>
2022-11-20 15:09:02 +0900

    [Refactor] : 무작위 난수를 문자열이 아닌 리스트로 반환

--- src/main/java/numberBaseballGame/GenerateRandomNumber.java
+++ src/main/java/numberBaseballGame/GenerateRandomNumber.java
@@ -1,25 +1,21 @@
 package numberBaseballGame;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
-public class generateRandomNumber {
+public class GenerateRandomNumber {
 
-    public StringBuilder createRandomNumber() {
-        Set<String> randomSet = new HashSet<>();
+    public List<Integer> createRandomNumber() {
+        Set<Integer> randomSet = new HashSet<>();
         String r = "";
 
         while (randomSet.size() < 3) {
             int d = (int) (Math.random() * 9 + 1);
 
-            randomSet.add(String.valueOf(d));
-        }
-        StringBuilder sb = new StringBuilder();
-
-        for (String idx : randomSet) {
-            sb.append(idx);
+            randomSet.add(d);
         }
 
-        return sb;
+        List<Integer> computerNumberList = new ArrayList<>(randomSet);
+
+        return computerNumberList;
     }
 }

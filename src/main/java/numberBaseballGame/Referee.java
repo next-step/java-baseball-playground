58218fc249e3cf3a8bb0a7ba5a17f110824662a4
YooDongSeon <inko51366@naver.com>
2022-11-21 00:32:30 +0900

    [Feat] : 스트라이크 갯수를 세어주는 메서드 추가

--- src/main/java/numberBaseballGame/Referee.java
+++ src/main/java/numberBaseballGame/Referee.java
@@ -1,4 +1,24 @@
 package numberBaseballGame;
 
+import java.util.List;
+
 public class Referee {
+    private List<Integer> computerNumberList;
+    private List<Integer> userNumberList;
+
+    public Referee(List<Integer> computerNumber, List<Integer> userNumber) {
+        this.computerNumberList = computerNumber;
+        this.userNumberList = userNumber;
+    }
+
+    public int strikeCount() {
+        int strike = 0;
+
+        for(Integer i : computerNumberList) {
+            if(computerNumberList.indexOf(i) == userNumberList.indexOf(i)) {
+                strike += 1;
+            }
+        }
+        return strike;
+    }
 }

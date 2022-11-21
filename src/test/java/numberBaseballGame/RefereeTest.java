f61404545c8883033a692fafd6bbf41856a9c975
YooDongSeon <inko51366@naver.com>
2022-11-21 13:58:08 +0900

    [Test] : @makeHint 메서드 테스트

--- src/test/java/numberBaseballGame/RefereeTest.java
+++ src/test/java/numberBaseballGame/RefereeTest.java
@@ -26,20 +26,24 @@ class RefereeTest {
     @Test
     @DisplayName("스트라이크 카운트가 정상적으로 올라가는지 테스트")
     public void strikeCount() {
-        referee = new Referee(computerNumber, userNumber);
-
         int answer = referee.strikeCount();
 
-        assertThat(answer).isEqualTo(2);
+        assertThat(answer).isEqualTo(3);
     }
 
     @Test
     @DisplayName("볼 카운트가 정상적으로 올라가는지 테스트")
     public void ballCount() {
-        referee = new Referee(computerNumber, userNumber);
-
         int answer = referee.ballCount();
 
         assertThat(answer).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("힌트 메시지가 출력되는지 테스트")
+    public void makeHints() {
+        String answer = referee.makeHint();
+
+        assertThat(answer).isEqualTo("1볼 1스트라이크");
     }
 }

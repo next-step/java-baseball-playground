7612ce9e881cf7e758b93f8943801dc016787dcc
YooDongSeon <inko51366@naver.com>
2022-11-21 18:18:01 +0900

    [Refactor] : 생성자를 통해 난수를 생성하게 변경

--- src/main/java/numberBaseballGame/GenerateRandomNumber.java
+++ src/main/java/numberBaseballGame/GenerateRandomNumber.java
@@ -3,10 +3,19 @@ package numberBaseballGame;
 import java.util.*;
 
 public class GenerateRandomNumber {
+    private List<Integer> computerNumber;
 
-    public List<Integer> createRandomNumber() {
+    public GenerateRandomNumber() {
+        List<Integer> randomNumber = this.createRandomNumber();
+        this.computerNumber = new ArrayList<>(randomNumber);
+    }
+
+    public List<Integer> getComputerNumber() {
+        return computerNumber;
+    }
+
+    List<Integer> createRandomNumber() {
         Set<Integer> randomSet = new HashSet<>();
-        String r = "";
 
         while (randomSet.size() < 3) {
             int d = (int) (Math.random() * 9 + 1);
@@ -14,8 +23,8 @@ public class GenerateRandomNumber {
             randomSet.add(d);
         }
 
-        List<Integer> computerNumberList = new ArrayList<>(randomSet);
+        List<Integer> computerList = new ArrayList<>(randomSet);
 
-        return computerNumberList;
+        return computerList;
     }
 }

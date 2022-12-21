1f1596557697c4697d274fecf861b798d75258bc
이재민 <rkwoals524@hanmail.net>
2022-12-21 21:57:15 +0900

    반복적인 연습, 작은 단위로 쪼개서 구현하는 연습

---
+++ src/main/java/baseball2/BallStatus2.java
@@ -0,0 +1,9 @@
+package baseball2;
+
+public enum BallStatus2 {
+
+    STRIKE,
+    BALL,
+    NOTHING,
+    ;
+}
--- src/test/java/baseball2/Ball2Test.java
+++ src/test/java/baseball2/Ball2Test.java
@@ -1,12 +1,15 @@
 package baseball2;
 
+import static baseball2.BallStatus2.BALL;
+import static baseball2.BallStatus2.NOTHING;
+import static baseball2.BallStatus2.STRIKE;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class BallTest {
+public class Ball2Test {
 
 //    같은 수가 같은 자리에 있으면 스트라이크, 다른 자리에 있으면 볼, 같은 수가 전혀 없으면 포볼 또는 낫싱이란 힌트를 얻고, 그 힌트를 이용해서 먼저 상대방(컴퓨터)의 수를 맞추면 승리한다.
 //        e.g. 상대방(컴퓨터)의 수가 425일 때, 123을 제시한 경우 : 1스트라이크, 456을 제시한 경우 : 1볼 1스트라이크, 789를 제시한 경우 : 낫싱
@@ -24,18 +27,18 @@ public class BallTest {
     @Test
     @DisplayName("STRIKE")
     void strike() {
-        assertThat(com.play(new Ball2(1, 1))).isEqualTo("strike");
+        assertThat(com.play(new Ball2(1, 1))).isEqualTo(STRIKE);
     }
 
     @Test
     @DisplayName("BALL")
     void ball() {
-        assertThat(com.play(new Ball2(2, 1))).isEqualTo("ball");
+        assertThat(com.play(new Ball2(2, 1))).isEqualTo(BALL);
     }
 
     @Test
     @DisplayName("NOTHING")
     void nothing() {
-        assertThat(com.play(new Ball2(3, 2))).isEqualTo("nothing");
+        assertThat(com.play(new Ball2(3, 2))).isEqualTo(NOTHING);
     }
 }

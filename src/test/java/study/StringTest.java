736fb3c7aea418015606e09f71ae055f914aa238
손효재 <sonjw5128@gmail.com>
2023-01-01 18:30:06 +0900

    refactor: variable name and java convention

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -15,15 +15,15 @@ public class StringTest {
 
     @Test
     void _1과_2가_나눠지는지_테스트() {
-        String[] str = "1,2".split(",");
-        assertThat(str).contains("1","2");
+        String[] text = "1,2".split(",");
+        assertThat(text).contains("1", "2");
     }
 
     @Test
     void substring_테스트() {
-        String str = "(1,2)";
-        String sub_str = str.substring(1, str.length()-1);
-        assertThat(sub_str).isEqualTo("1,2");
+        String text = "(1,2)";
+        String substringText = text.substring(1, text.length() - 1);
+        assertThat(substringText).isEqualTo("1,2");
     }
 
     @Test

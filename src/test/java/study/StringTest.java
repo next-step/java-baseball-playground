89fa6b438bf5c2a0088152feefec022aad973fe0
손효재 <sonjw5128@gmail.com>
2023-01-01 18:23:52 +0900

    test: charAt에 대한 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,10 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
@@ -22,5 +24,23 @@ public class StringTest {
         String str = "(1,2)";
         String sub_str = str.substring(1, str.length()-1);
         assertThat(sub_str).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정위치 문자 가져오기 테스트")
+    void charAt_테스트() {
+        String text = "abc";
+        assertThat(text.charAt(0)).isEqualTo('a');
+    }
+
+    @Test
+    @DisplayName("위치 값을 벗어나면 StringIndexOutOfBoundsException 테스트")
+    void charAt_indexOut_테스트() {
+        String text = "abc";
+
+        assertThatThrownBy(() -> {
+            text.charAt(text.length());
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range");
     }
 }

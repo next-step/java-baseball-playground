50495db31edcf0a6445bcf0a8e8aea8ea56d1b6e
pcy <cas15987@gmail.com>
2023-01-03 11:35:46 +0900

    String Test 코드 구현

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,52 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
+    /*
+        AssertJ Exception Assertions 문서 참고
+        https://joel-costigliola.github.io/assertj/assertj-core-features-highlight.html#exception-assertion
+    */
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        final String value = "1,2";
+        final String[] actual = value.split(",");
+        assertThat(actual).containsExactly("1", "2");
+    }
+
+    @Test
+    void substring() {
+        final String value = "(1,2)";
+        final String actual = value.substring(1, value.length() - 1);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("특정 위치 문자 가져오기[실패] - 위치 값 벗어남")
+    void charAt_ThrownBy() {
+        final String value = "abc";
+        final int len = value.length();
+        assertThatThrownBy(() -> value.charAt(len))
+                .isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: " + len);
+    }
+
+    @Test
+    @DisplayName("특정 위치 문자 가져오기[실패] - 위치 값 벗어남")
+    void charAt_ExceptionOfType() {
+        final String value = "abc";
+        final int len = value.length();
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+                .isThrownBy(() -> value.charAt(len))
+                .withMessageMatching("String index out of range: " + len);
     }
 }

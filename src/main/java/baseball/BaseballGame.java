e631d0fa22620051005806d253fd561a24824bf9
pcy <cas15987@gmail.com>
2023-01-05 20:28:56 +0900

    feat: Compare Number

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -40,4 +40,45 @@ public class BaseballGame {
             throw new IllegalStateException("숫자를 입력해주세요");
         }
     }
+
+    // 3. 정답과 입력한 수를 비교한다.
+    // 3-1. 볼인지 확인한다.
+    public int checkBall(final String number,
+                         final String inputNumber) {
+        int ball = 0;
+
+        final char first = number.charAt(0);
+        final char second = number.charAt(1);
+        final char third = number.charAt(2);
+
+        if (inputNumber.charAt(0) == second || inputNumber.charAt(0) == third) {
+            ball++;
+        }
+        if (inputNumber.charAt(1) == first || inputNumber.charAt(1) == third) {
+            ball++;
+        }
+        if (inputNumber.charAt(2) == first || inputNumber.charAt(2) == second) {
+            ball++;
+        }
+
+        return ball;
+    }
+
+    // 3-2. 스트라이크인지 확인한다.
+    public int checkStrike(final String number,
+                           final String inputNumber) {
+        int strike = 0;
+
+        if (number.charAt(0) == inputNumber.charAt(0)) {
+            strike++;
+        }
+        if (number.charAt(1) == inputNumber.charAt(1)) {
+            strike++;
+        }
+        if (number.charAt(2) == inputNumber.charAt(2)) {
+            strike++;
+        }
+
+        return strike;
+    }
 }

be50610fc1250f8eac12348d064ded7afbc20bec
pcy <cas15987@gmail.com>
2023-01-05 20:18:07 +0900

    feat: Get Input Number

--- src/main/java/baseball/BaseballGame.java
+++ src/main/java/baseball/BaseballGame.java
@@ -2,6 +2,7 @@ package baseball;
 
 import java.util.HashSet;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,5 +17,27 @@ public class BaseballGame {
         }
 
         return set.stream().map(String::valueOf).collect(Collectors.joining());
+    }
+
+    // 2. 숫자를 입력 받는다.
+    public String getInputNumber() {
+        System.out.print("숫자를 입력해 주세요 : ");
+
+        // 2-1. 문자열을 입력 받는다.
+        final Scanner kb = new Scanner(System.in);
+        final String inputString = kb.nextLine();
+
+        // 2-2. 숫자인지 검증한다.
+        isNumber(inputString);
+
+        return inputString.trim();
+    }
+
+    private void isNumber(final String inputString) {
+        try {
+            Integer.parseInt(inputString);
+        } catch (NumberFormatException e) {
+            throw new IllegalStateException("숫자를 입력해주세요");
+        }
     }
 }

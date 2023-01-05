7e4ac5a32a87b67bc71cf96995efcfaa6ec5971e
xx10222 <yjw4960@gmail.com>
2023-01-05 17:13:26 +0900

    :sparkles: [Step2] 숫자야구 시작

---
+++ src/main/java/baseball/Baseball.java
@@ -0,0 +1,12 @@
+package baseball;
+
+import java.util.Scanner;
+
+public class Baseball {
+    public String input() {
+        Scanner scanner = new Scanner(System.in);
+        return scanner.nextLine();
+    }
+
+    public
+}

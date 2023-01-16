326151d866003b4f8d010a2ee85061f7b60d587a
ssongone <41146676+ssongone@users.noreply.github.com>
2023-01-17 02:44:44 +0900

    Feat : Add Round.java
    Round 클래스: 핵심 서비스
      -스트라이크,볼,아웃,홈런 체크하는 메소드 작성
    
    자바 플레이그라운드 1 숫자야구
    2단계: 숫자야구 게임 구현

---
+++ src/main/java/Round.java
@@ -0,0 +1,60 @@
+import java.util.ArrayList;
+
+public class Round {
+
+    private long ball = 0;
+    private long strike = 0;
+    private boolean isOut = false;
+
+    public Round() { //테스트용
+
+    }
+
+    public Round(ArrayList<Integer> correctAnswer, ArrayList<Integer> inputs) {
+        ball = checkBall(correctAnswer, inputs);
+        strike = checkStrike(correctAnswer, inputs);
+        isOut = checkOut(correctAnswer, inputs);
+    }
+
+    long checkStrike(ArrayList<Integer> correctAnswer, ArrayList<Integer> inputs) {
+        long strikeCount = correctAnswer.stream()
+                .filter(answer -> answer == inputs.get(correctAnswer.indexOf(answer)))
+                .count();
+        return strikeCount;
+    }
+
+    long checkBall(ArrayList<Integer> correctAnswer, ArrayList<Integer> inputs) {
+        long ballCount = correctAnswer.stream()
+                .filter(answer -> inputs.stream().anyMatch(input->input==answer))
+                .filter(answer -> answer != inputs.get(correctAnswer.indexOf(answer)))
+                .count();
+        return ballCount;
+    }
+
+    boolean checkOut(ArrayList<Integer> correctAnswer, ArrayList<Integer> inputs) {
+        long count = correctAnswer.stream()
+                .filter(answer -> inputs.stream().noneMatch(input -> answer == input))
+                .count();
+        if(count==3)
+            return true;
+        return false;
+    }
+
+    boolean isHomeRun(long strike) {
+        if(strike==3)
+            return true;
+        return false;
+    }
+
+    public long getBall() {
+        return ball;
+    }
+
+    public long getStrike() {
+        return strike;
+    }
+
+    public boolean isOut() {
+        return isOut;
+    }
+}

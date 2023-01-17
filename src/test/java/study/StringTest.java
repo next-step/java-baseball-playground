afc4654c9e3e83de22420bceca70f9a1a0ebb9b1
ororso11 <ororso135055@gmail.com>
2023-01-17 22:34:38 +0900

    첫번쨰 단위테스트
    1. 요구사항 1 -> split 기능이 정상작동하는지에 대한 테스트코드 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,11 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    // 요구사항 1
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual[0]).isEqualTo("1");
+    }
 }

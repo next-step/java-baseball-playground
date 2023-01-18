55c8dd11f3473c9add84b4bb44e36da69f61107d
ssongone <41146676+ssongone@users.noreply.github.com>
2023-01-18 16:23:09 +0900

    Test : Add BaseballGameTest.java
    정답설정테스트(): 정답이 중복없이 잘 설정됐는지 확인
    
    자바 플레이그라운드 1 숫자야구
    2단계: 숫자야구 게임 구현

---
+++ src/test/java/BaseballGameTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BaseballGameTest {
+
+    InputService is = new InputService();
+    BaseballGame game = new BaseballGame(is);
+
+
+//    @Test
+//    void 통합테스트() throws IOException {
+//        game.start();
+//
+//    }
+
+    @Test
+    void 정답설정테스트() {
+        game.setSolution();
+        System.out.println(game.getSolution().toString());
+
+        assertThat(game.getSolution().size()).isEqualTo(3);
+    }
+
+}

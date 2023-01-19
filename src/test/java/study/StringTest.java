a8d2c1b543cc9a26f913c32bb2dea9719f46a10c
ororso11 <ororso135055@gmail.com>
2023-01-19 23:54:08 +0900

    첫번쨰 단위테스트
    1. 요구사항 1 -> split 기능이 정상작동하는지에 대한 테스트코드 작성

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -2,6 +2,7 @@ package study;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
@@ -17,4 +18,33 @@ public class StringTest {
         String[] actual = "1,2".split(",");
         assertThat(actual[0]).isEqualTo("1");
     }
+
+    @Test
+    void contains() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains(actual[0],actual[1]);
+    }
+
+    @Test
+    void containsExactly() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).containsExactly(actual[0],actual[1]);
+    }
+
+    // 요구사항 2
+    @Test
+    void substring() {
+        String str = "(1,2)".substring(1,4);
+        assertThat(str).isEqualTo("1,2");
+    }
+
+    // 요구사항 3
+    @Test
+    void charAt() {
+        char oneChar = "123".charAt(0);
+        System.out.println(oneChar);
+    }
+
+
+
 }

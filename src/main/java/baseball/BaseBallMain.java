ff1925ef7f9a69e54b782265fbccfed4f4ebd21d
ororso11 <ororso135055@gmail.com>
2023-02-02 00:31:04 +0900

    단위테스트 코드 작성
    1. 숫자 야구게임 굴러가게 만드는 버전
    
    고쳐야할점
    1. 규칙 적용
    2. 단위 테스트 미비

--- src/main/java/baseball/BaseBallMain.java
+++ src/main/java/baseball/BaseBallMain.java
@@ -5,87 +5,85 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class BaseBallMain {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        String[] computerRandomNumbers = new String[3];
-        String computerRandomNumber = "";
+    public static String[] computerRandomNumbers = new String[3];
+    public static String[] userRandomNumbers = new String[3];
 
+    public static int ballNum = 0;
+    public static int strike = 0;
+    public static boolean isReturnGame = true;
+    public static int isReturnNumber = 0;
+    public static Scanner sc = new Scanner(System.in);
+
+    public static String inputValue() {
+        return sc.next();
+    }
+
+    public static void getComputerRandomNumbers() {
         for(int i = 0; i < computerRandomNumbers.length;i++) {
             computerRandomNumbers[i] = String.valueOf((int)(Math.random() * 9) + 1);
+            for(int j = 0; j < i; j++) {
+                if(computerRandomNumbers[i].equals(computerRandomNumbers[j])) {
+                    i--;
+                }
+            }
         }
+        getComputerRandomNumbersTrans(computerRandomNumbers);
+    }
 
-        computerRandomNumber = Arrays.stream(computerRandomNumbers)
-                .collect(Collectors.joining());
+    public static void getComputerRandomNumbersTrans(String[] computerRandomNumbers) {
+        String resComputerRandomNumbers = Arrays.stream(BaseBallMain.computerRandomNumbers).collect(Collectors.joining());
+        print(resComputerRandomNumbers);
+    }
 
-        System.out.println(" 컴퓨터 : " + computerRandomNumber);
+    public static void characterComparison(String userInputNumbers) {
+        for(int i = 0; i < computerRandomNumbers.length;i++) {
+            if(userInputNumbers.contains(computerRandomNumbers[i])) {
+                indexNumberComparison(userInputNumbers.indexOf(computerRandomNumbers[i]),i);
+            }
+        }
+    }
 
-        String[] userInputNumbers = new String[3];
+    public static void indexNumberComparison(int indexNumbers, int i ) {
+        if( indexNumbers == i ) {
+            strike++;
+        } else {
+            ballNum++;
+        }
+    }
+
+    public static void numberBaseBallIsBoolean() {
+        if( strike == 3 ) {
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            isReturnNumber = Integer.parseInt(inputValue());
+            gameChoiceNumbers(isReturnNumber);
+        }
+    }
+
+    public static void gameChoiceNumbers(int isReturnNumber) {
+        if(isReturnNumber == 2) {
+            isReturnGame = false;
+            return;
+        }
+        getComputerRandomNumbers();
+    }
+
+    public static void print(String resComputerRandomNumbers) {
+        System.out.println("컴퓨터 :" + resComputerRandomNumbers);
+    }
+
+    public static void main(String[] args) {
+        getComputerRandomNumbers();
+
         String userInputNumber = "";
 
-        int indexNumbers = 0;
-
-        int ballNum = 0;
-        int strike = 0;
-
-        int isReturnNumber = 0;
-
-        boolean isReturnGame = true;
-
         while ( isReturnGame ) {
-
             System.out.print("숫자를 입력해 주세요 : ");
-            userInputNumber = sc.next();
-
-            if(userInputNumber.contains(computerRandomNumbers[0])) {
-                indexNumbers = userInputNumber.indexOf(computerRandomNumbers[0]);
-                if( indexNumbers == 0 ) {
-                    strike++;
-                } else if( indexNumbers != 0 ) {
-                    ballNum++;
-                }
-            }
-            if(userInputNumber.contains(computerRandomNumbers[1])) {
-                indexNumbers = userInputNumber.indexOf(computerRandomNumbers[1]);
-                if( indexNumbers == 1 ) {
-                    strike++;
-                } else if( indexNumbers != 1 ) {
-                    ballNum++;
-                }
-            }
-            if(userInputNumber.contains(computerRandomNumbers[2])) {
-                indexNumbers = userInputNumber.indexOf(computerRandomNumbers[2]);
-                if( indexNumbers == 2 ) {
-                    strike++;
-                } else if( indexNumbers != 2 ) {
-                    ballNum++;
-                }
-            }
-            if( userInputNumber.contains(computerRandomNumbers[0]) && userInputNumber.contains(computerRandomNumbers[1]) &&
-                    userInputNumber.contains(computerRandomNumbers[2])){
-                ballNum = 3;
-            }
-
-            if( strike == 3 ) {
-                System.out.println(ballNum + "볼 " + strike + "스트라이크");
-                System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-                System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-                isReturnNumber = sc.nextInt();
-
-                if(isReturnNumber == 2) {
-                    isReturnGame = false;
-                    break;
-                } else if( isReturnNumber == 1 ){
-                    for(int i = 0; i < computerRandomNumbers.length;i++) {
-                        computerRandomNumbers[i] = String.valueOf((int)(Math.random() * 9) + 1);
-                    }
-                    computerRandomNumber = Arrays.stream(computerRandomNumbers).collect(Collectors.joining());
-                    System.out.println(" 컴퓨터 : " + computerRandomNumber);
-                }
-
-            } else if(strike != 3) {
-                System.out.println(ballNum + "볼 " + strike + "스트라이크");
-            }
+            String inputNum = inputValue();
+            characterComparison(inputNum);
+            System.out.println(ballNum + "볼 " + strike + "스트라이크");
+            numberBaseBallIsBoolean();
             ballNum = 0;
             strike = 0;
         }

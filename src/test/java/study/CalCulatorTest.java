5250b3f4c2b36343359d83dd0bf2d1df02b964c1
yoonjaej3 <yoonjaej3@gmail.com>
2023-02-27 15:08:19 +0900

    리펙토링

--- src/test/java/study/CalCulatorTest.java
+++ src/test/java/study/CalCulatorTest.java
@@ -28,38 +28,59 @@ public class CalCulatorTest {
         String[] values = value.split(" ");
 
         //when
-
-        int result = Integer.parseInt(values[0]);
-
-        for (int i = 1; i < values.length-1; i+=2) {
-
-            int rightHandSide = Integer.parseInt(values[i + 1]);
-
-            switch (values[i]) {
-                case "+":
-                    result += rightHandSide;
-                    break;
-                case "-":
-                    result -= rightHandSide;
-                    break;
-                case "*":
-                    result *= rightHandSide;
-                    break;
-                case "/":
-                    try {
-                        result /= rightHandSide;
-                    } catch (ArithmeticException e) {
-                        System.out.println("0으로 나눌 수 없습니다.");
-                    }
-                    break;
-                default:
-                    throw new IllegalArgumentException("잘못된 연산자입니다.");
-            }
-
-        }
+        int result = calculate(values);
 
         //then
         Assertions.assertEquals(result,10);
 
+    }
+
+    private static int calculate(String[] values) {
+
+        int leftHandSide = Integer.parseInt(values[0]);
+
+        int result = leftHandSide ;
+
+        for(int i = 1; i< values.length-1; i+=2){
+
+            int rightHandSide = Integer.parseInt(values[i +1]);
+
+            result = calcluateValue(values[i], result, rightHandSide);
+
+        }
+
+        return result;
+    }
+
+    private static int calcluateValue(String operator, int leftHandSide, int rightHandSide) {
+
+        switch (operator){
+
+            case "+":
+
+                return leftHandSide + rightHandSide;
+
+            case "-":
+
+                return leftHandSide - rightHandSide;
+
+            case "*":
+
+                return leftHandSide * rightHandSide;
+
+            case "/":
+
+                if (rightHandSide == 0) {
+
+                    throw new ArithmeticException("0으로 나눌 수 없습니다.");
+
+                }
+
+                return leftHandSide / rightHandSide;
+            default:
+
+                throw new IllegalArgumentException("잘못된 연산자입니다.");
+
+        }
     }
 }

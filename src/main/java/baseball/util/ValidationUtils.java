d55a354bcddc1a438f211490db7e2ce61fa787c4
giljin <kiljin1012@naver.com>
2023-03-02 20:24:34 +0900

    피드백 후 다시 개발 구현

---
+++ src/main/java/baseball/util/ValidationUtils.java
@@ -0,0 +1,24 @@
+package baseball.util;
+
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class ValidationUtils {
+
+    private static final String NUMBER_REGEX = "^[0-9]*$";
+
+    public static boolean isNumber(String text) {
+        return Pattern.matches(NUMBER_REGEX, text);
+    }
+
+    public static boolean macthLength(String text, int length) {
+        return text.length() == length;
+    }
+
+    public static boolean isDuplicationNumber(List<Integer> numberList) {
+        long distinctCount = numberList.stream()
+                .distinct()
+                .count();
+        return numberList.size() == distinctCount;
+    }
+}

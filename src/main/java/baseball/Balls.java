63f6aa8ab2988b1e7e17760b803022ccca428582
giljin <kiljin1012@naver.com>
2023-03-07 21:31:51 +0900

    유효성검사 추가

--- src/main/java/baseball/Balls.java
+++ src/main/java/baseball/Balls.java
@@ -6,13 +6,21 @@ import java.util.List;
 public class Balls {
 
     private final List<Ball> results;
+    private final int MAX_SIZE = 3;
     public Balls(List<Integer> numbers) {
+        checkSize(numbers);
         this.results = mapBall(numbers);
+    }
+
+    private void checkSize(List<Integer> numbers){
+        if(numbers.size() > MAX_SIZE){
+            throw new IllegalArgumentException("볼 사이즈가 올바르지 않습니다");
+        }
     }
 
     private List<Ball> mapBall(List<Integer> numbers){
         List<Ball> balls = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < MAX_SIZE; i++) {
             Ball ball = new Ball(i+1, numbers.get(i));
             balls.add(ball);
         }
@@ -29,7 +37,7 @@ public class Balls {
 
     public BaseBallResult play(Balls userBalls) {
         BaseBallResult result = new BaseBallResult();
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < MAX_SIZE; i++) {
             BallStatus status = userBalls.compare(this.results.get(i));
             result.add(status);
         }

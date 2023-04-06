e64cb79812c1c4a0c124270eae3b547746468fb0
truelightwater <truelightwater@gmail.com>
2023-04-06 12:07:23 +0900

    String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,21 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String[] values = "1,2".split(",");
+        assertThat(values).containsExactly("1","2");
+
+        values = "1".split(",");
+        assertThat(values).contains("1");
+    }
+
+    @Test
+    void subString() {
+        String input = "(1,2)";
+        String substring = input.substring(1, input.length() - 1);
+        assertThat(substring).isEqualTo("1,2");
+
+    }
 }

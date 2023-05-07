04e5b2b198cb2c4f238cfba11dcf3a4a8bae457d
qwe5507 <apvmffkdls@gmail.com>
2023-05-07 23:05:13 +0900

    문자열 계산기 구현 2차
    리뷰어 블로그 참고하여 내가 구현한 코드에도 적용

--- src/main/java/Operator.java
+++ src/main/java/Operator.java
@@ -1,6 +1,6 @@
-import java.util.*;
+import java.util.Collections;
+import java.util.Map;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -8,26 +8,39 @@ public enum Operator {
     PLUS("+", (first, second) -> first + second),
     MINUS("-", (first, second) -> first - second),
     MULTIPLY("*", (first, second) -> first * second),
-    DIVIDE("/", (first, second) -> first / second);
+    DIVIDE("/", (first, second) -> {
+        if (second == 0) {
+            throw new IllegalArgumentException();
+        }
+        return first / second;
+    });
 
-    private String symbol;
-    private BiFunction<Integer, Integer, Integer> operation;
+    private static final Map<String, Operator> OPERATOR_MAP =
+            Collections.unmodifiableMap(Stream.of(values())
+                    .collect(Collectors.toMap(operator -> operator.getSymbol(), operator -> operator)));
+    private final String symbol;
+    private final BiFunction<Integer, Integer, Integer> operation;
 
-    Operator(String symbol, BiFunction<Integer, Integer, Integer> operation){
+    Operator(String symbol, BiFunction<Integer, Integer, Integer> operation) {
         this.symbol = symbol;
         this.operation = operation;
+    }
+
+    public static Operator findOperator(String symbol) {
+        Operator operator = OPERATOR_MAP.get(symbol);
+
+        if (operator == null) {
+            throw new IllegalArgumentException();
+        }
+
+        return operator;
     }
 
     public String getSymbol() {
         return symbol;
     }
 
-    public int operate(int first, int second){
+    public int operate(int first, int second) {
         return operation.apply(first, second);
     }
-
-    private static final Map<String, Operator> operatorMap =
-            Collections.unmodifiableMap(Stream.of(values())
-                    .collect(Collectors.toMap(Operator::getSymbol, Function.identity())));
-
 }
--- src/main/java/StringCalc.java
+++ src/main/java/StringCalc.java
@@ -1,43 +1,27 @@
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.regex.Pattern;
 
 public class StringCalc {
-    static final String NUMBER_REGEXP = "^[0-9]*$";
-    public int calc(String calcString) {
-        String[] values = calcString.split(" ");
+    private static final Pattern NUMBER_REGEXP = Pattern.compile("^[0-9]*$");
+    private Formula formula;
 
-        int sum = Integer.valueOf(values[0]);
-        String symbol = "+";
-        for (int i = 1; i < values.length; i++) {
+    public StringCalc(Formula formula) {
+        this.formula = formula;
+    }
 
+    public int calc() {
 
-            if(Pattern.matches(NUMBER_REGEXP, values[i])){
+        int sum = 0;
+        Operator currentOperator = Operator.PLUS;
 
-            }
-//            if (symbolMap.contains(values[i])) {
-//                symbol = values[i];
-//                continue;
-//            }
-
-            int targetNumber = Integer.valueOf(values[i]);
-            switch (symbol) {
-                case "+":
-                    sum = sum + targetNumber;
-                    break;
-                case "-":
-                    sum = sum - targetNumber;
-                    break;
-                case "*":
-                    sum = sum * targetNumber;
-                    break;
-                case "/":
-                    sum = sum / targetNumber;
-                    break;
+        for (String input : formula.getFormula()) {
+            if (NUMBER_REGEXP.matcher(input).find()) {
+                sum = currentOperator.operate(sum, Integer.valueOf(input));
+                continue;
             }
 
+            currentOperator = Operator.findOperator(input);
         }
+
         return sum;
     }
 }

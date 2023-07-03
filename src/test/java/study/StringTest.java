a8d4c477b766aee23591d398f6615283a78854c6
kimbro97 <rlagudwog@gmail.com>
2023-07-03 20:11:47 +0900

    요구사항 1, 2

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,4 +11,22 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        String str = "1,2";
+        String[] split = str.split(",");
+        System.out.println(split[0]);
+        assertThat(split[0]).isEqualTo("1");
+        assertThat(split[1]).isEqualTo("2");
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException")
+    void substring() {
+        String str = "(1,2)";
+        String substring = str.substring(1, 4);
+        assertThat(substring).isEqualTo("1,2");
+    }
+
 }

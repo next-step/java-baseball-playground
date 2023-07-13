e05b33dbba43ae7152ad133d7463750436a5c457
Firedrago95 <yonghwa95@naver.com>
2023-07-13 20:52:28 +0900

    string 계산기 기능구현 (리팩토링 x)

---
+++ src/main/java/StringCal.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+public class StringCal {
+    public static String inputExpression () {
+        return (new Scanner(System.in)).nextLine();
+    }
+
+    public static String [] splitExpression(String expression) {
+        String [] values = expression.split(" ");
+        return values;
+    }
+
+    public static float castingFloat(String value) {
+        return Float.parseFloat(value);
+    }
+    public static int castingInt(String value) {
+        return Integer.parseInt(value);
+    }
+
+    public static float plus (float calculationResult , int nextTerm) {
+        return calculationResult+nextTerm;
+    }
+
+    public static float minus (float calculationResult , int nextTerm) {
+        return calculationResult-nextTerm;
+    }
+
+    public static float divide (float calculationResult , int nextTerm) {
+        return (float)calculationResult/nextTerm;
+    }
+
+    public static float multiply (float calculationResult , int nextTerm) {
+        return calculationResult*nextTerm;
+    }
+    public static void main(String[] args) {
+        System.out.println("식을 입력하세요");
+        String value = inputExpression();
+        String [] values = splitExpression(value);
+        float calculationResult = castingFloat(values[0]);
+
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].equals("+")) {
+                int nextTerm = castingInt(values[i+1]);
+                calculationResult = plus(calculationResult, nextTerm);
+            }
+            if (values[i].equals("-")) {
+                int nextTerm = castingInt(values[i+1]);
+                calculationResult = minus(calculationResult, nextTerm);
+            }
+            if (values[i].equals("/")) {
+                int nextTerm = castingInt(values[i+1]);
+                calculationResult = divide(calculationResult, nextTerm);
+            }
+            if (values[i].equals("*")) {
+                int nextTerm = castingInt(values[i+1]);
+                calculationResult = multiply(calculationResult, nextTerm);
+            }
+        }
+        System.out.println(calculationResult);
+    }
+}

2c46b460d76351efc089e2c80b631e63a8a096b4
Firedrago95 <yonghwa95@naver.com>
2023-07-13 20:59:27 +0900

    string 계산기 기능구현 (리팩토링 x)

--- src/main/java/StringCalculator/StringCal.java
+++ src/main/java/StringCalculator/StringCal.java
@@ -1,3 +1,5 @@
+package StringCalculator;
+
 import java.util.*;
 
 public class StringCal {
@@ -16,7 +18,27 @@ public class StringCal {
     public static int castingInt(String value) {
         return Integer.parseInt(value);
     }
-
+    public static float calculator (String [] values, float calculationResult) {
+        for (int i = 0; i < values.length; i++) {
+            if (values[i].equals("+")) {
+                int nextTerm = castingInt(values[i+1]);
+                calculationResult = plus(calculationResult, nextTerm);
+            }
+            if (values[i].equals("-")) {
+                int nextTerm = castingInt(values[i+1]);
+                calculationResult = minus(calculationResult, nextTerm);
+            }
+            if (values[i].equals("/")) {
+                int nextTerm = castingInt(values[i+1]);
+                calculationResult = divide(calculationResult, nextTerm);
+            }
+            if (values[i].equals("*")) {
+                int nextTerm = castingInt(values[i+1]);
+                calculationResult = multiply(calculationResult, nextTerm);
+            }
+        }
+        return calculationResult;
+    }
     public static float plus (float calculationResult , int nextTerm) {
         return calculationResult+nextTerm;
     }
@@ -36,26 +58,8 @@ public class StringCal {
         System.out.println("식을 입력하세요");
         String value = inputExpression();
         String [] values = splitExpression(value);
-        float calculationResult = castingFloat(values[0]);
-
-        for (int i = 0; i < values.length; i++) {
-            if (values[i].equals("+")) {
-                int nextTerm = castingInt(values[i+1]);
-                calculationResult = plus(calculationResult, nextTerm);
-            }
-            if (values[i].equals("-")) {
-                int nextTerm = castingInt(values[i+1]);
-                calculationResult = minus(calculationResult, nextTerm);
-            }
-            if (values[i].equals("/")) {
-                int nextTerm = castingInt(values[i+1]);
-                calculationResult = divide(calculationResult, nextTerm);
-            }
-            if (values[i].equals("*")) {
-                int nextTerm = castingInt(values[i+1]);
-                calculationResult = multiply(calculationResult, nextTerm);
-            }
-        }
-        System.out.println(calculationResult);
+        float firstValue = castingFloat(values[0]);
+        float answer = calculator(values, firstValue);
+        System.out.println(answer);
     }
 }

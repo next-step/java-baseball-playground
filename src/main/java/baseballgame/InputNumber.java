f1e00525a96b2094f74cd4825fd9f4f1433f7220
ekore <yonghwa95@naver.com>
2023-07-19 20:43:03 +0900

    유효성 검사 validNum( ) 완성

--- src/main/java/baseballgame/InputNumber.java
+++ src/main/java/baseballgame/InputNumber.java
@@ -1,18 +1,21 @@
 package baseballgame;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 public class InputNumber {
     public String inputUserNum() {
         System.out.println("1~9 중복없는 세자리 숫자를 입력해주세요");
         return new Scanner(System.in).nextLine();
     }
-//    public static ArrayList validNum(String number) {
-//       if(isNum(number) && isDuplicate(number) && isThreeDigit(number)) {
-//            ArrayList list = addList(parsing(number));
-//       }
-//    }
+    public static ArrayList validNum(String number) {
+       if(isSingleDigitNum(number) && isUnique(number) && isThreeDigits(number)) {
+            return addList(parsing(number));
+       }
+       return null;
+    }
 
     private static String[] parsing(String number) {
         return number.split("");
@@ -20,17 +23,16 @@ public class InputNumber {
     private static ArrayList addList (String [] numbers) {
         ArrayList list = new ArrayList();
         for (int i = 0; i < 3; i++) {
-            list.add(numbers[i]);
+            list.add(Integer.parseInt(numbers[i]));
         }
         return list;
     }
 
-    public static boolean isDigitNum (String number) {
-        boolean digitNum = false;
+    public static boolean isSingleDigitNum (String number) {
         if (isNum(number)) {
-            digitNum = isOneDigit(number);
+            return isOneDigit(number);
         }
-        return digitNum;
+        return false;
     }
 
     private static boolean isNum (String number) {
@@ -58,4 +60,24 @@ public class InputNumber {
         }
         return false;
     }
+
+    public static boolean isUnique(String number) {
+        HashSet<Character> uniqueChars = new HashSet<>();
+        for (char ch : number.toCharArray()) {
+            if (!uniqueChars.add(ch)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean isThreeDigits (String number) {
+        if (number.length() == 3) {
+            return true;
+        }
+        return false;
+    }
+
+
+
 }

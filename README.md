20ef4e60add2c231385af62b2475cb55aa4ccd80
ekore <yonghwa95@naver.com>
2023-07-19 20:45:45 +0900

    유효성 검사 validNum( ) 테스트 케이스 작성완료

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 1. [v]컴퓨터의 1~9 중복없는 세자리 숫자를 생성한다.  generateComputerNum()
 2. [v]사용자로부터 1~9 중복없는 세자리 숫자를 입력받는다. inputUserNum()
-3. []입력받은 숫자의 유효성 검사 통과하면 ArrayList 담아서 반환 validNum()
+3. [v]입력받은 숫자의 유효성 검사 통과하면 ArrayList 담아서 반환 validNum()
     - [v]1~9 사이의 숫자인지 확인한다 isDigitNum( )
-    - []중복된 값이 없는지 확인한다. isDuplicate( )
-    - []세자리 숫자인지 확인한다. isThreeDigit()
+    - [v]중복된 값이 없는지 확인한다. isUnique( )
+    - [v]세자리 숫자인지 확인한다. isThreeDigit()
 - []위치값과 숫자값이 같으면 Strike 
 - []위치값은 다르지만 숫자값이 같으면 ball
 - []위치값과 숫자값이 모두 다르면 nothing
--- src/test/java/baseball/InputNumberTest.java
+++ src/test/java/baseball/InputNumberTest.java
@@ -13,18 +13,32 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 public class InputNumberTest {
 
 
-//    @Test
-//    @DisplayName("유효성 검사 통과하면 ArrayList 반환")
-//    void 유효성_검사_검증() {
-//        assertThat(InputNumber.validNum("713")).isEqualTo(Arrays.asList(7,1,3));
-//    }
+    @Test
+    void validNumTest() {
+        assertThat(InputNumber.validNum("713")).isEqualTo(Arrays.asList(7,1,3));
+        assertThat(InputNumber.validNum("711")).isEqualTo(null);
+    }
 
     @Test
     void isDigitTest() {
-        assertThat(InputNumber.isDigitNum("d12")).isFalse();
-        assertThat(InputNumber.isDigitNum("1d1")).isFalse();
-        assertThat(InputNumber.isDigitNum("11d")).isFalse();
-        assertThat(InputNumber.isDigitNum("111")).isTrue();
-        assertThat(InputNumber.isDigitNum("583")).isTrue();
+        assertThat(InputNumber.isSingleDigitNum("d12")).isFalse();
+        assertThat(InputNumber.isSingleDigitNum("1d2")).isFalse();
+        assertThat(InputNumber.isSingleDigitNum("12d")).isFalse();
+        assertThat(InputNumber.isSingleDigitNum("123")).isTrue();
+        assertThat(InputNumber.isSingleDigitNum("456")).isTrue();
+    }
+
+    @Test
+    void isUniqueTest() {
+        assertThat(InputNumber.isUnique("112")).isFalse();
+        assertThat(InputNumber.isUnique("344")).isFalse();
+        assertThat(InputNumber.isUnique("123")).isTrue();
+        assertThat(InputNumber.isUnique("131")).isFalse();
+    }
+
+    @Test
+    void isThreeDigitsTest() {
+        assertThat(InputNumber.isThreeDigits("111")).isTrue();
+        assertThat(InputNumber.isThreeDigits("1234")).isFalse();
     }
 }

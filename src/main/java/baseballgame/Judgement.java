f195d2b3d42d7d5d2c7b34cca64e8eaf812625ad
ekore <yonghwa95@naver.com>
2023-07-23 10:34:13 +0900

    변경점 푸쉬

---
+++ src/main/java/baseballgame/Judgement.java
@@ -0,0 +1,21 @@
+﻿package baseballgame;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Judgement {
+    public int isContainNumber(ArrayList computer, ArrayList user) {
+        int count = 0;
+        for (int index = 0; index < user.size(); index++) {
+            int userNum = (int)user.get(index);
+            if(computer.contains(userNum)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public boolean isCheckNumber(ArrayList computer, int index , int userNum) {
+        return (int)computer.get(index) == userNum;
+    }
+}
---
+++ src/main/java/baseballgame/Refree.java
@@ -0,0 +1,21 @@
+﻿package baseballgame;
+
+import java.util.ArrayList;
+
+public class Refree {
+    int strike = 0;
+    int ball = 0;
+
+    Judgement judgement = new Judgement();
+
+    public void makeCall(ArrayList computer,ArrayList user) {
+      int count = judgement.isContainNumber(computer, user);
+      for (int index = 0; index < user.size(); index++) {
+          int userNum = (int)user.get(index);
+          if(judgement.isCheckNumber(computer,index, userNum)) {
+              strike++;
+          }
+      }
+      ball = count - strike;
+    }
+}

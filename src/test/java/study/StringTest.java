09780a3f356745d2b29e33774e5b5bdfa4d0289a
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-08-02 18:42:16 +0900

    jonghoon test

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,17 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void test(){
+        assertTrue(true);
     }
 }

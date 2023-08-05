c5c30e76053da115e5bd095bc1a94230e4126d90
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-08-05 14:39:52 +0900

    test : Operator test 코드 작성

---
+++ src/test/java/OperatorTest.java
@@ -0,0 +1,18 @@
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class OperatorTest {
+
+    @Test
+    @DisplayName("연산자는 연산을 할 수 있습니다.")
+    void operate() {
+        // given // when
+        String expression = "1 + 3";
+
+        // then
+        assertThat(Operator.PLUS.function(1.0, 3.0)).isEqualTo(4);
+    }
+
+}

ebdd6828283c84fb8e38e891f71fc39fbbfaff21
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2023-08-05 14:49:11 +0900

    refactor : Operator 코드 function 메소드 수정 및 테스트 코드 추가

--- src/main/java/Operator.java
+++ src/main/java/Operator.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public enum Operator {
@@ -9,7 +8,12 @@ public enum Operator {
     PLUS("+", (a, b) -> a + b),
     MINUS("-", (a, b) -> a - b),
     MULTIPLY("*", (a, b) -> a * b),
-    DIVIDE("/", (a, b) -> a / b);
+    DIVIDE("/", (a, b) -> {
+        if (b == 0.0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
+        return a / b;
+    });
 
     private final String value;
     private final BiFunction<Double, Double, Double> function;
--- src/test/java/OperatorTest.java
+++ src/test/java/OperatorTest.java
@@ -1,4 +1,5 @@
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -6,12 +7,50 @@ import org.junit.jupiter.api.Test;
 class OperatorTest {
 
     @Test
-    @DisplayName("연산자는 연산을 할 수 있습니다.")
-    void function() {
+    @DisplayName("연산자는 덧셈을 할 수 있습니다.")
+    void plus() {
         // given // when
         String expression = "1 + 3";
         // then
-        assertThat(Operator.PLUS.function(1.0, 3.0)).isEqualTo(4);
+        assertThat(Operator.PLUS.function(1.0, 3.0)).isEqualTo(1 + 3);
+    }
+
+    @Test
+    @DisplayName("연산자는 뺄셈을 할 수 있습니다.")
+    void minus() {
+        // given // when
+        String expression = "1 - 3";
+        // then
+        assertThat(Operator.MINUS.function(1.0, 3.0)).isEqualTo(1 - 3);
+    }
+
+    @Test
+    @DisplayName("연산자는 곱셈을 할 수 있습니다.")
+    void multiply() {
+        // given // when
+        String expression = "1 * 3";
+        // then
+        assertThat(Operator.MULTIPLY.function(1.0, 3.0)).isEqualTo(1 * 3);
+    }
+
+    @Test
+    @DisplayName("연산자는 나눗셈을 할 수 있습니다.")
+    void divide() {
+        // given // when
+        String expression = "1 / 3";
+        // then
+        assertThat(Operator.DIVIDE.function(1.0, 3.0)).isEqualTo(1.0 / 3.0);
+    }
+
+    @Test
+    @DisplayName("연산자는 0으로 나눌 수 없습니다.")
+    void divideFail() {
+        // given // when
+        String expression = "1 / 0";
+        // then
+        assertThatThrownBy(() -> Operator.DIVIDE.function(1.0, 0.0))
+            .isInstanceOf(ArithmeticException.class)
+            .hasMessage("0으로 나눌 수 없습니다.");
     }
 
 }

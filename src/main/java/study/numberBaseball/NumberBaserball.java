43f5a78a0f55bf0ab1a9d873e6aa5f04d9ddf1a3
DESKTOP-TJJRH84\whdgn <whdgns5059@gmail.com>
2023-08-06 19:36:21 +0900

    숫자야구 작성 완료

--- src/main/java/study/numberBaseball/NumberBaserball.java
+++ src/main/java/study/numberBaseball/NumberBaserball.java
@@ -4,16 +4,10 @@ import java.util.Scanner;
 
 public class NumberBaserball {
 
-    int[] number;
-    int[] input;
 
-    public NumberBaserball(){
-        number = initNumber();
-    }
+    public int initNumber() {
 
-    public int[] initNumber() {
-
-        int[] num = makeNumber();
+        int num = makeNumber();
 
         while(hasSameNumber(num)){
             num = makeNumber();
@@ -23,43 +17,101 @@ public class NumberBaserball {
 
     }
 
-    public boolean hasSameNumber(int[] num) {
-
-        return num[0] == num[1]
-                || num[1] == num[2]
-                || num[2] == num[0];
-    }
-
-    public int[] makeNumber() {
-
-        int[] num = new int[3];
-
-        for(int i = 0 ; i < 3 ; i++){
-            num[i] = (int)(Math.random() * 10);
+    public boolean hasSameNumber(int num) {
+        int[] nums = new int[3];
+        int index = 0;
+        while(num > 0){
+            nums[index++] = num % 10;
+            num /= 10;
         }
 
-        return num;
+        return nums[0] == nums[1]
+                || nums[1] == nums[2]
+                || nums[2] == nums[0];
     }
 
-   public void scanNumber() {
-       Scanner sc = new Scanner(System.in);
-       int num = sc.nextInt();
+    public int makeNumber() {
+        return (int)(Math.random() * 1000);
+    }
 
-       int[] result = new int[3];
-       int i = 2;
-       while (num > 0) {
-           result[i--] = num % 10;
-           num /= 10;
-       }
-       this.input = result;
+   public int scanNumber(Scanner sc) {
+
+       System.out.print("숫자를 입력해 주세요 : ");
+       return sc.nextInt();
    }
 
-   public String guessTheAnswer(){
+    public int countBall(int number, int input) {
 
-        int
+        int[] numbers = numberToArray(number);
+        int[] inputs = numberToArray(input);
+
+        int count = 0;
+        if(numbers[0] == inputs[1] || numbers[0] == inputs[2]){
+            count += 1;
+        }
+        if(numbers[1] == inputs[2] || numbers[1] == inputs[0]){
+            count += 1;
+        }
+        if(numbers[2] == inputs[0] || numbers[2] == inputs[1]){
+            count += 1;
+        }
+
+        return count;
+    }
+
+    public int[] numberToArray(int number) {
+        int[] result = new int[3];
+        for(int i = 2 ; i >= 0 ; i--){
+            result[i] = number % 10;
+            number /= 10;
+        }
+        return result;
+    }
+
+    public int countStrike(int number, int input) {
+        int[] numbers = numberToArray(number);
+        int[] inputs = numberToArray(input);
+        int count = 0;
+        for(int i = 0 ; i < inputs.length ; i++){
+            count += numbers[i] == inputs[i] ? 1 : 0;
+        }
+        return count;
+    }
+
+    public String printResult(int ballCount, int strikeCount) {
+
+        StringBuilder sb = new StringBuilder();
+        if(strikeCount == 3){
+            sb.append("3스트라이크\n");
+            sb.append("3개의 숫자를 모두 맞히셧습니다! 게임종료");
+
+            return sb.toString();
+        }
 
 
+        if(ballCount > 0){
+            sb.append(ballCount).append("볼");
+        }
+        if(ballCount > 0 && strikeCount > 0){
+            sb.append(" ");
+        }
+        if(strikeCount > 0){
+            sb.append(strikeCount).append("스트라이크");
+        }
+        if(ballCount == 0 && strikeCount == 0){
+            sb.append("아무것도 없습니다.");
+        }
 
-   }
+        return sb.toString();
 
+    }
+
+    public int contiueGame(Scanner sc) {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return sc.nextInt();
+    }
+
+    public void printGameOver() {
+        System.out.println("게임종료!");
+    }
 }
--- src/test/java/study/numberBaseball/NumberBaseballTest.java
+++ src/test/java/study/numberBaseball/NumberBaseballTest.java
@@ -31,6 +31,28 @@ public class NumberBaseballTest {
 
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {"123:123:0","123:321:2","583:358:3","794:421:1"}, delimiter = ':')
+    void countBall(int number, int input, int expected){
+        int actual = baserball.countBall(number, input);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"123:123:3","567:561:2","953:243:1","521:251:1"},delimiter = ':')
+    void countStrike(int number, int input, int expected){
+        int actual = baserball.countStrike(number, input);
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:2:1볼 2스트라이크","2:1:2볼 1스트라이크"
+                ,"3:0:3볼", "0:0:아무것도 없습니다."}, delimiter = ':')
+    void printResult(int ball, int strike, String expeced){
+        String actual = baserball.printResult(ball, strike);
+        Assertions.assertThat(actual).matches(expeced);
+    }
+
 
 
 }

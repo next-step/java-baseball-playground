47aafbc404493fcc3e3a8b6d28d2a43efc05f4b4
rmaWhrdl <wwscan3@gmail.com>
2023-08-11 17:50:05 +0900

    feat(baseball) : 뷰와 도메인을 연결해줄 컨트롤러 구현

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,2 +1,33 @@
-package baseball.controller;public class BaseballGameController {
+package baseball.controller;
+
+import baseball.domain.*;
+import baseball.ui.InputView;
+import baseball.ui.ResultView;
+
+import java.util.List;
+
+public class BaseballGameController {
+    static NumberGenerator numberGenerator = new NumberGenerator();
+    static InputView inputView = new InputView();
+    static GameStatus gameStatus;
+
+    public void playGame() {
+        do {
+            List<GameNumber> computerNumbers = numberGenerator.createGameNumbers(numberGenerator.createRandomNumbers());
+            matchNumbers(computerNumbers);
+
+        } while(gameStatus.restartOrQuit(inputView.restartOrQuitInput()));
+    }
+
+    private void matchNumbers(List<GameNumber> computerNumbers) {
+        Referee referee = new Referee();
+        ResultView resultView = new ResultView();
+        List<GameNumber> userNumbers;
+
+        do {
+            userNumbers = numberGenerator.createUserNumbers(inputView.receiveInput());
+            gameStatus = referee.compare(computerNumbers, userNumbers);
+
+        } while (resultView.giveResult(gameStatus));
+    }
 }

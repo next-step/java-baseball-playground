fac8393346a70fd71b0fa5f1fe859f55e4cd20b2
rmaWhrdl <wwscan3@gmail.com>
2023-08-11 16:56:46 +0900

    feat(baseball) : 컴퓨터의 수와 플레이어의 수를 비교할 수 있는 기능 구현

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -3,8 +3,19 @@ package baseball.domain;
 import java.util.List;
 
 public class Referee {
-    public String compare(List<Integer> computer, List<Integer> user) {
-        return null;
+    public GameStatus compare(List<GameNumber> computer, List<GameNumber> user) {
+        final Judgement judgement = new Judgement();
+        int correctCount = judgement.correctCount(computer, user);
+
+        int strike = 0;
+        for (int i = 0 ; i < computer.size(); i++) {
+            if(judgement.hasPlace(computer, i, user.get(i).getNumber())) {
+                strike++;
+            }
+        }
+        int ball = correctCount-strike;
+
+        return new GameStatus(strike, ball);
     }
 
 }

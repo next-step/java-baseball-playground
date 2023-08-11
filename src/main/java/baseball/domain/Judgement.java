e6cf175401c21c0705a524630a40263bb551f02b
rmaWhrdl <wwscan3@gmail.com>
2023-08-11 14:44:52 +0900

    feat(baseball) : create skeleton code

---
+++ src/main/java/baseball/domain/Judgement.java
@@ -0,0 +1,14 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Judgement {
+    public int correctCount(List<Integer> computer, List<Integer> user) {
+        return 0;
+    }
+
+    public boolean hasPlace(int place, int number) {
+        return false;
+    }
+
+}

5f48cc6b620bd50964aaf1661c55c17fa50cc722
rmaWhrdl <wwscan3@gmail.com>
2023-08-11 17:49:43 +0900

    feat(baseball) : 숫자야구게임 입력과 결과값을 출력할 뷰 구현

--- src/main/java/baseball/ui/InputView.java
+++ src/main/java/baseball/ui/InputView.java
@@ -1,2 +1,21 @@
-package baseball.ui;public class InputView {
+package baseball.ui;
+
+import java.util.Scanner;
+
+public class InputView {
+    static final Scanner sc = new Scanner(System.in);
+
+    public String receiveInput() {
+        System.out.println("숫자를 입력해 주세요 : ");
+        String input = sc.nextLine();
+        if(input.isEmpty()) {
+            throw new IllegalArgumentException("입력값이 없습니다.");
+        }
+        return input;
+    }
+
+    public int restartOrQuitInput() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Integer.parseInt(sc.nextLine());
+    }
 }
--- src/main/java/baseball/ui/ResultView.java
+++ src/main/java/baseball/ui/ResultView.java
@@ -1,2 +1,19 @@
-package baseball.ui;public class ResultView {
+package baseball.ui;
+
+import baseball.domain.GameStatus;
+
+public class ResultView {
+    public boolean giveResult(GameStatus gameStatus) {
+        if (gameStatus.getBall() == 3) {
+            System.out.println("낫싱");
+            return true;
+        }
+        if (gameStatus.getStrike() == 3) {
+            System.out.println("3스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            return false;
+        }
+        System.out.println(gameStatus.getBall()+"볼 "+gameStatus.getStrike()+"스트라이크");
+        return true;
+    }
 }

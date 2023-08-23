b6ee2cd4af74970463ce3f64a94d1d726e0d9bb9
dan <a41792998@gmail.com>
2023-08-23 12:29:07 +0900

    계산이 제대로 되지않는 에러 해결

--- src/test/java/study/StringCalculator.java
+++ src/test/java/study/StringCalculator.java
@@ -5,6 +5,11 @@ import java.util.StringTokenizer;
 
 public class StringCalculator {
 
+    /** 에러 1 .  계산식이 토큰으로 뽑히지 않음
+     *  해결   .  StringTokenizer stringTokenizer = new StringTokenizer() 3번째 인자에 true 추가
+     *           3번째 인자는 구분자로 토큰을 분리할 때 구분자도 토큰에 포함시킬지 여부 default값은 false
+     * **/
+
     public static void main(String[] args){
         String token = "";
         int amount = 0;
@@ -14,7 +19,7 @@ public class StringCalculator {
         System.out.println("계산식 입력 : ");
 
         String inputData = sc.nextLine();
-        StringTokenizer stringTokenizer = new StringTokenizer(inputData, "+-*/");
+        StringTokenizer stringTokenizer = new StringTokenizer(inputData, "+-*/", true);
 
         try {
             while (stringTokenizer.hasMoreTokens()){

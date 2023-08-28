082e547839438826a25e65b72add4f36c4d96e9c
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-08-28 23:08:06 +0900

    리팩토링/주석/테스트추가

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -7,14 +7,23 @@ public class BaseballGame {
 
     private static int SIZE = 3;
 
+    /**
+     * 시작
+     */
     public void init() {
+
+        //1. 난수 생성
         LinkedHashSet<Integer> randomNums = makeRandomNumbers();
         boolean flag = false;
         while(!flag) {
+            //2. 사용자 숫자 입력
             String numString = _getInputNumString();
+
+            //3. 정답 확인
             flag = tryAnswer(numString, randomNums);
         }
 
+        //4. 재시작 입력
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
         System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         Scanner scanner = new Scanner(System.in);
@@ -23,6 +32,10 @@ public class BaseballGame {
         if (restartFlag.equals("1")) init();
     }
 
+    /**
+     * 난수 생성
+     * @return
+     */
     public LinkedHashSet<Integer> makeRandomNumbers() {
         LinkedHashSet<Integer> randomNums = new LinkedHashSet<>();
         Random rd = new Random();
@@ -32,22 +45,36 @@ public class BaseballGame {
         return randomNums;
     }
 
+    /**
+     * 정답 시도
+     * @param numString
+     * @param randomNums
+     * @return
+     */
     public boolean tryAnswer(String numString, LinkedHashSet<Integer> randomNums) {
 
-        HashMap<String, Integer> count = new HashMap<>();
-        count.put("ball", 0);
-        count.put("strike", 0);
+        //1. ball,strike Counter
+        HashMap<String, Integer> counter = _initCounter();
 
         for (int i=0; i<numString.length(); i++) {
+            //2. 사용자 숫자 한글자씩 자름
             Integer num = Integer.parseInt(numString.substring(i,i+1));
-            _countBallOrStrike(count, randomNums, num, i);
+
+            //3. ball or strike 판별
+            countBallOrStrike(counter, randomNums, num, i);
         }
 
-        _printResult(count);
+        //4. count 결과 출력
+        _printResult(counter);
 
-        return count.get("strike") == SIZE;
+        //5. 정답 여부
+        return counter.get("strike") == SIZE;
     }
 
+    /**
+     * 사용자 숫자 입력
+     * @return
+     */
     private String _getInputNumString() {
         System.out.print("숫자를 입력해 주세요 : ");
         Scanner scanner = new Scanner(System.in);
@@ -55,17 +82,28 @@ public class BaseballGame {
         return numString;
     }
 
-    private void _countBallOrStrike(HashMap<String, Integer> count, LinkedHashSet<Integer> randomNums, Integer num, int index) {
+    /**
+     * ball or strike 판별
+     * @param counter
+     * @param randomNums
+     * @param num
+     * @param index
+     */
+    public void countBallOrStrike(HashMap<String, Integer> counter, LinkedHashSet<Integer> randomNums, Integer num, int index) {
         Integer[] randomNumArray = randomNums.toArray(new Integer[randomNums.size()]);
         if (randomNumArray[index] == num) {
-            count.put("strike",count.get("strike")+1);
+            counter.put("strike",counter.get("strike")+1);
             return;
         }
         if (randomNums.contains(num)) {
-            count.put("ball", count.get("ball")+1);
+            counter.put("ball", counter.get("ball")+1);
         }
     }
 
+    /**
+     * 결과 출력
+     * @param count
+     */
     private void _printResult(HashMap<String, Integer> count) {
         if(count.get("ball") > 0) {
             System.out.print(count.get("ball")+"볼 ");
@@ -75,6 +113,18 @@ public class BaseballGame {
             System.out.print(count.get("strike")+"스트라이크");
         }
         System.out.println();
+    }
+
+    /**
+     * Counter 초기화
+     * @return
+     */
+    private HashMap<String, Integer> _initCounter() {
+        HashMap<String, Integer> counter = new HashMap<>();
+        counter.put("ball", 0);
+        counter.put("strike", 0);
+
+        return counter;
     }
 
 

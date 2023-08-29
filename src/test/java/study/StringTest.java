f11febeaf793dfa7dec415d7a157a51535b4e868
sung-ik-je <ij16_21@naver.com>
2023-08-29 20:46:17 +0900

    (1,2) 요구사항까지

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -5,9 +5,28 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+    @Test
+    void split() {
+        String[] actual = "1,2".split(",");
+        assertThat(actual).contains(new String[]{"1", "2"});
+    }
+
+    @Test
+    void split2(){
+        String[] actual = "1,".split(",");
+        assertThat(actual).contains(new String[]{"1"});
+    }
+
+    @Test
+    void substring(){
+        String actual = "(1,2)".substring(1, 4);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
 }

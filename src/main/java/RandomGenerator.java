a5d485647eeca0882f91c4c75cca64e1c0708c43
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-09-03 23:59:17 +0900

    난수생성 기능

--- src/main/java/RandomGenerator.java
+++ src/main/java/RandomGenerator.java
@@ -1,5 +1,8 @@
+import java.util.Random;
+
 public class RandomGenerator {
     public void createRandomNums() {
-
+        Random rd = new Random();
+        int randomNum = rd.nextInt(9)+1;
     }
 }

c1d96871023151834520b4efe549ffdeeb85fdf8
nahyun0423 <skgus0423@naver.com>
2023-09-03 21:53:25 +0900

    week1_1

---
+++ src/test/java/study/StringCalculator.java
@@ -0,0 +1,28 @@
+package study;
+
+import java.util.Scanner;
+
+public class StringCalculator {
+    Scanner scanner = new Scanner(System.in)
+    String value = scanner.nextLine();
+    String[] values = value.split(" ");
+
+
+    public int add (int a, int b) {
+        return a + b;
+    }
+    public int sub (int a, int b) {
+        return a + b;
+    }
+    public int multiply (int a, int b) {
+        return a + b;
+    }
+    public int divide (int a, int b) {
+        return a + b;
+    }
+
+
+    int number = Integer.parseInt(value);
+}
+
+
--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,8 @@
 package study;
 
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +12,34 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split() {
+        String value = "1,2";
+        String[] splitedValue = value.split(",");
+        assertThat(splitedValue).contains("1", "2");
+    }
+
+    @Test
+    void substring() {
+        String value = "1,2".substring(1, 4);
+        assertThat(value).isEqualTo("1,2");
+    }
+
+    @Test
+    @DisplayName("use charAt()")
+    void charAt() {
+        String value = "abc";
+        char result = value.charAt(1);
+        assertThat(result).isEqualTo('b');
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 발생")
+    void StringIndexOutOfBoundsException() {
+        String value = "abc";
+        char result = value.charAt(10);
+        assertThat(result).isEqualTo('c');
     }
 }

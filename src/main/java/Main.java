fa230a5ef8d9e4354f297e1306c0347ed09081b0
Kyuyoung11 <58154672+Kyuyoung11@users.noreply.github.com>
2023-09-06 00:21:42 +0900

    main에 합치기

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,12 @@
 import java.util.LinkedHashSet;
 
 public class Main {
+    private static BallsGenerator ballsGenerator = new BallsGenerator();
     public static void main(String[] args) {
         //1. 난수 생성
-        LinkedHashSet<Integer> randomNums = _getRandomNums();
-    }
+        Balls balls = ballsGenerator.makeRandomBalls(3);
 
-    private static LinkedHashSet<Integer> _getRandomNums() {
-        RandomGenerator rdGenerator = new RandomGenerator();
-        return rdGenerator.createRandomNums();
+        //2. 사용자 숫자 입력 받기
+
     }
 }

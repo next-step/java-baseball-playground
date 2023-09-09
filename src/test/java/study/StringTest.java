1f986a87a78d97b4b0a4aa63d26633e32ffc32e1
sangdoll <pricelees@naver.com>
2023-09-09 11:44:26 +0900

    요구사항1 반영

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,5 +1,6 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,5 +10,20 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+    @Test
+    @DisplayName("문자열을 split 했을 때 잘 분리되는지 확인")
+    void split(){
+        // Given
+        String str1 = "1,2";
+        String str2 = "1";
+
+        // When
+        String[] actual = str1.split(",");
+        String[] actual1 = str2.split(",");
+
+        // Then
+        assertThat(actual).containsExactly("1","2");
+        assertThat(actual1).containsExactly("1");
     }
 }

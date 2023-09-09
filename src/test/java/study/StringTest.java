ff1073a600c4d93fa834da192b248b5989e058a6
sangdoll <pricelees@naver.com>
2023-09-09 11:46:14 +0900

    요구사항 2 반영

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -26,4 +26,17 @@ public class StringTest {
         assertThat(actual).containsExactly("1","2");
         assertThat(actual1).containsExactly("1");
     }
+
+    @Test
+    @DisplayName("문자의 괄호가 잘 지워지는지 확인")
+    void substring(){
+        // Given
+        String input = "(1,2)";
+
+        // When
+        String actual = input.substring(1,input.length()-1);
+
+        // Then
+        assertThat(actual).isEqualTo("1,2");
+    }
 }

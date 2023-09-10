f3aed3bc73c95cb673654cec45fa9fba8513cfa3
sangdoll <pricelees@naver.com>
2023-09-10 17:15:45 +0900

    [Feat] 입력 UI를 관리하는 InputView 클래스를 추가했습니다.

---
+++ src/main/java/InputView.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class InputView {
+    private final Scanner scanner;
+
+    public InputView(){
+        scanner = new Scanner(System.in);
+    }
+
+    public String getInputNumber(){
+        System.out.print("숫자를 입력해 주세요 : ");
+        return scanner.nextLine();
+    }
+    public int restartOrExit() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        return Integer.parseInt(scanner.nextLine());
+    }
+}

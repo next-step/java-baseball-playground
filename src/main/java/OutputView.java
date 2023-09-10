e702f2922b9010edb6e43fb5d9e5e834aede4222
sangdoll <pricelees@naver.com>
2023-09-10 17:16:27 +0900

    [Feat] 출력 UI를 관리하는 OutputVIew 클래스를 추가했습니다.

---
+++ src/main/java/OutputView.java
@@ -0,0 +1,10 @@
+public class OutputView {
+    public void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public void printGameEndMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료.");
+    }
+
+}

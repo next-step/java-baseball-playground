f94c4a2e261d6813713e6e31bd2bf5d25f722a26
sangdoll <pricelees@naver.com>
2023-09-11 23:24:04 +0900

    [Test] 두 개의 공을 받아 볼 / 스트라이크 / 낫싱 여부를 확인합니다.

---
+++ src/test/java/newBaseball/BallTest.java
@@ -0,0 +1,30 @@
+package newBaseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class BallTest {
+    @Test
+    @DisplayName("스트라이크 결과를 얻어내는지 확인합니다.")
+    void getResult_isStrike_true() {
+        Ball answer = new Ball(1,4);
+        Ball input = new Ball(1,4);
+        assertThat(answer.getResult(input)).isEqualTo(PitchStatus.STRIKE);
+    }
+    @Test
+    @DisplayName("볼 결과를 얻어내는지 확인합니다.")
+    void ball() {
+        Ball answer = new Ball(1,4);
+        Ball input = new Ball(2,4);
+        assertThat(answer.getResult(input)).isEqualTo(PitchStatus.BALL);
+    }
+    @Test
+    @DisplayName("낫싱 결과를 얻어내는지 확인합니다.")
+    void nothing() {
+        Ball answer = new Ball(1,4);
+        Ball input = new Ball(2,3);
+        assertThat(answer.getResult(input)).isEqualTo(PitchStatus.NOTHING);
+    }
+}

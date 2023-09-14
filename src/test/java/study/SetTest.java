2f5a2e2f82a7942f0ee9602ac6c54d2d19fa7f91
JungInBaek <bji1527@naver.com>
2023-09-15 07:57:20 +0900

    test : Set 클래스에 대한 학습 테스트
    - size(), contains() 테스트 작성

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,48 @@
+package study;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    @Test
+    @DisplayName("size()로 Set 사이즈 확인")
+    void size() {
+        int actual = numbers.size();
+        assertThat(actual).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    @DisplayName("contains()로 Set 내부값 존재 확인, 중복 제거")
+    void contains(Integer input) {
+        assertThat(numbers.contains(input));
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1:true", "2:true", "3:true", "4:false", "5:false"}, delimiter = ':')
+    void contains2(Integer input, Boolean expected) {
+        boolean actual = numbers.contains(input);
+        assertThat(actual).isEqualTo(expected);
+    }
+}

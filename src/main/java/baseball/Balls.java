490d7dcf51fd633986b1aec4439788525c9ae223
borussia444423 <jung78978978@naver.com>
2023-10-18 08:55:32 +0900

    TTD 숫자야구구현게임2

---
+++ src/main/java/baseball/Balls.java
@@ -0,0 +1,29 @@
+package baseball;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Balls {
+    private final List<Ball> balls;
+
+    public Balls(List<Integer> answers) {
+        this.balls = mapBall(answers);
+    }
+
+    private static List<Ball> mapBall(List<Integer> answers) {
+        List<Ball> balls = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            balls.add(new Ball(i + 1, answers.get(i)));
+        }
+        return balls;
+    }
+
+    public BallStatus play(Ball userBall) {
+        return balls.stream()
+                .map(answer -> answer.play(userBall))
+                .filter(BallStatus::isNotNothing)
+                .findFirst()
+                .orElse(BallStatus.NOTHING);
+    }
+}

9e11d344f424367ba0fbd8838adee89b17b50b53
borussia444423 <jung78978978@naver.com>
2023-10-17 23:42:55 +0900

    TDD 숫자 야구 게임 구현 첫번째

--- src/test/java/baseball/ValidationUtilsTest.java
+++ src/test/java/baseball/ValidationUtilsTest.java
@@ -5,6 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+/**
+ * 위치와 숫자 같이 같은 경우 -> strike
+ * 위치는 다른데, 숫자 값이 같은 경우 -> ball
+ * 숫자 값이 다른 경우 -> nothing
+ *
+ * com / user
+ * 1 4 / 1 4 -> Strike
+ * 1 4 / 2 4 -> Ball
+ * 1 4 / 2 5 -> Nothing
+ */
+
 public class ValidationUtilsTest {
     @Test
     @DisplayName("야구_숫자_1_9_검증")

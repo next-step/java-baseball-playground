4960cdbae8887d6404d02a2c7f19dc06aa8f79f2
choi14123 <ckdghks5565@gmail.com>
2023-12-26 22:19:40 +0900

    feat: "BaseballController 기능 구현"

---
+++ src/main/java/baseball/controller/BaseballController.java
@@ -0,0 +1,38 @@
+package baseball.controller;
+
+import baseball.model.BaseballNumbers;
+import baseball.model.RandomNumbers;
+import baseball.model.Referee;
+import baseball.model.UserNumbers;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.Scanner;
+
+public class BaseballController {
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    RandomNumbers randomNumbers = new RandomNumbers();
+    UserNumbers userNumbers = new UserNumbers();
+
+    public void start() {
+        int number = 0;
+        Scanner scanner = new Scanner(System.in);
+        outputView.printStartGameMessage();
+        while (number != 2) {
+            BaseballNumbers baseballNumbers = new BaseballNumbers(randomNumbers.create());
+            inputView.printInputNumbersMessage();
+            //System.out.println(baseballNumbers.toString());
+            Referee referee = new Referee(baseballNumbers, userNumbers.inputUserNumbers());
+            outputView.printRefereeMessage(referee.getBall(), referee.getStrike());
+            if (referee.getStrike() == 3) {
+                inputView.printInputGameRestartMessage(referee.getStrike());
+                number = scanner.nextInt();
+                randomNumbers.deleteComputerNumbers();
+            }
+            userNumbers.deleteUserInputNumbers();
+            referee.deleteBallStrikeCount();
+        }
+    }
+}

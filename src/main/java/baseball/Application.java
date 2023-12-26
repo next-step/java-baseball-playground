5b97d76f2683b8995c1cd2744db76835b11e8317
choi14123 <ckdghks5565@gmail.com>
2023-12-26 22:19:49 +0900

    feat: "Application 기능 구현"

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,10 @@
 package baseball;
 
+import baseball.controller.BaseballController;
+
 public class Application {
     public static void main(String[] args) {
-
+        BaseballController baseballController = new BaseballController();
+        baseballController.start();
     }
 }

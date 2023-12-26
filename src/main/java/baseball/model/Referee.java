81a770ff6fbc2e1b3fcdac42b320d6a063416c72
choi14123 <ckdghks5565@gmail.com>
2023-12-26 22:17:26 +0900

    feat: "Referee 기능 구현"

---
+++ src/main/java/baseball/model/Referee.java
@@ -0,0 +1,51 @@
+package baseball.model;
+
+public class Referee {
+    private static int strike = 0;
+    private static int ball = 0;
+
+    public Referee(BaseballNumbers computer, BaseballNumbers user) {
+        countBall(computer, user);
+        countStrike(computer, user);
+    }
+
+    public static int getBall() {
+        return ball;
+    }
+
+    public static int getStrike() {
+        return strike;
+    }
+
+    private int countBall(BaseballNumbers computer, BaseballNumbers user) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (computer.getNumbers().get(i).getNumber() == user.getNumbers().get(j).getNumber()) {
+                    if (i != j) {
+                        ball++;
+                    }
+                }
+            }
+        }
+        return ball;
+    }
+
+
+    private int countStrike(BaseballNumbers computer, BaseballNumbers user) {
+        for (int i = 0; i < 3; i++) {
+            if (computer.getNumbers().get(i).getNumber() == user.getNumbers().get(i).getNumber()) {
+                strike++;
+            }
+        }
+        return strike;
+    }
+
+    public static int isGameEnd() {
+        return 3;
+    }
+
+    public void deleteBallStrikeCount() {
+        ball = 0;
+        strike = 0;
+    }
+}

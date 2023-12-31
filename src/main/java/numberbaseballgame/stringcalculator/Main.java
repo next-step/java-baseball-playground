db98614efc353b83126c992411cd8a61b09e6d95
Hyungjun Lim <hotfirebulldog@gmail.com>
2023-12-31 18:11:42 +0900

    [feat]문자열계산기 버전1을 구현한다

---
+++ src/main/java/numberbaseballgame/stringcalculator/Main.java
@@ -0,0 +1,26 @@
+package numberbaseballgame.stringcalculator;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String value = scanner.nextLine();
+        String[] values = value.split(" ");
+
+        int sum = Integer.parseInt(values[0]);
+        for (int i = 1; i < values.length; i++) {
+            if (i % 2 != 0) {
+                switch (values[i]) {
+                    case "+" -> sum += Integer.parseInt(values[i + 1]);
+                    case "-" -> sum -= Integer.parseInt(values[i + 1]);
+                    case "*" -> sum *= Integer.parseInt(values[i + 1]);
+                    case "/" -> sum /= Integer.parseInt(values[i + 1]);
+                    default -> System.out.println("잘못된 연산자입니다.");
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+}

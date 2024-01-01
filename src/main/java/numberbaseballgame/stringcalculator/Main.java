d3ab3d365b3b50f3ded76e5132b982a4d72944f2
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-01-01 12:49:10 +0900

    [feat]계산을 수행할 때 생기는 예외들을 처리를 한다

--- src/main/java/numberbaseballgame/stringcalculator/Main.java
+++ src/main/java/numberbaseballgame/stringcalculator/Main.java
@@ -4,32 +4,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String value = scanner.nextLine();
-        String[] values = value.split(" ");
 
-        int sum = Integer.parseInt(values[0]);
-        for (int i = 1; i < values.length; i++) {
-            if (i % 2 != 0) {
-                switch (values[i]) {
-                    case "+" -> sum += Integer.parseInt(values[i + 1]);
-                    case "-" -> sum -= Integer.parseInt(values[i + 1]);
-                    case "*" -> sum *= Integer.parseInt(values[i + 1]);
-                    case "/" -> {
-                        try {
-                            sum /= Integer.parseInt(values[i + 1]);
-                        } catch (ArithmeticException e) {
-                            System.out.println("0으로 나눌 수 없습니다.");
-                            System.exit(1);
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("계산식을 입력하세요.(예 ) 3 + 4 * 2 / 2)");
+            String value = scanner.nextLine();
+            String[] values = value.split(" ");
+
+            try {
+                int sum = Integer.parseInt(values[0]);
+                for (int i = 1; i < values.length; i++) {
+                    if (i % 2 != 0) {
+                        switch (values[i]) {
+                            case "+" -> sum += Integer.parseInt(values[i + 1]);
+                            case "-" -> sum -= Integer.parseInt(values[i + 1]);
+                            case "*" -> sum *= Integer.parseInt(values[i + 1]);
+                            case "/" -> sum /= Integer.parseInt(values[i + 1]);
+                            default -> System.out.println("잘못된 연산자입니다.");
                         }
                     }
-                    default -> {
-                        System.out.println("잘못된 연산자입니다.");
-                        System.exit(1);
-                    }
                 }
+                System.out.println("계산결과 : " + sum);
+                break;
+            } catch (ArithmeticException e) {
+                System.out.println("0으로 나눌 수 없습니다.");
+            } catch (NumberFormatException e) {
+                System.out.println("숫자를 입력해주세요.");
+            } catch (ArrayIndexOutOfBoundsException e) {
+                System.out.println("잘못된 입력입니다.");
             }
         }
-        System.out.println(sum);
     }
 }

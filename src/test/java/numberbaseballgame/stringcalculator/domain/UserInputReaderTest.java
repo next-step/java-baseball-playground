dced05e009c02ac53d0e8e8ab0d0bf500e655179
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-01-02 19:24:30 +0900

    [test]splitBy테스트코드에 @DisplayName을 적용한다

--- src/test/java/numberbaseballgame/stringcalculator/domain/UserInputReaderTest.java
+++ src/test/java/numberbaseballgame/stringcalculator/domain/UserInputReaderTest.java
@@ -1,16 +1,15 @@
 package numberbaseballgame.stringcalculator.domain;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 class UserInputReaderTest {
 
     UserInputReader userInputReader = new UserInputReader();
 
+    @DisplayName("입력받은 문자열을 특정한 문자를 기준으로 나눈다")
     @Test
     void splitBy() {
         assertThat(userInputReader.splitBy("1 + 2 * 3", " "))

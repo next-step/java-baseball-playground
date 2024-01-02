cfd33aaafc92bbb032141ab3170f759885bef7d2
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-01-02 18:54:01 +0900

    [feat]입력받은 문자열을 공백을 기준으로 나눈다

--- src/main/java/numberbaseballgame/stringcalculator/domain/UserInputReader.java
+++ src/main/java/numberbaseballgame/stringcalculator/domain/UserInputReader.java
@@ -1,6 +1,8 @@
 package numberbaseballgame.stringcalculator.domain;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 import java.util.regex.Pattern;
 
@@ -9,36 +11,15 @@ public class UserInputReader {
     private static final Pattern intPattern = Pattern.compile("^-?\\d+$");
 
     // 이렇게 변수를 두개를 나누니 복잡도가 올라감. Calculate에서 한번에 처리하고 싶은데 나눠서 처리할 수 밖에 없게 됨
-    ArrayList<Integer> numbers = new ArrayList<>();
-    ArrayList<Character> operators = new ArrayList<>();
-
-    // 입력값을 읽어들인다
-    public void read() {
-        separateNumberAndOperator(splitInput(getInput()));
-    }
-
-    // 문자열을 입력받는다
-    public String getInput() {
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextLine();
-    }
+//    ArrayList<Integer> numbers = new ArrayList<>();
+//    ArrayList<Character> operators = new ArrayList<>();
+    private List<String> userInput;
 
     // 입력받은 문자열을 공백을 기준으로 나눈다
-    public String[] splitInput(String input) {
-        return input.split(" ");
-    }
-
-    // 나눈 문자열을 숫자와 연산자로 구분한다
-    public void separateNumberAndOperator(String[] components) {
-        for (String component : components) {
-            if (isNumberic(component)) {
-                numbers.add(Integer.parseInt(component));
-            }
-
-            if (!isNumberic(component)) {
-                operators.add(component.charAt(0));
-            }
-        }
+    public List<String> splitBy(String input, String character) {
+        String[] inputs = input.split(character);
+        userInput.addAll(Arrays.asList(inputs));
+        return userInput;
     }
 
     // 숫자인지 확인한다

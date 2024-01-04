e2a9ff1935080fa9e08bca0600faf500d7efca0f
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-01-04 15:48:32 +0900

    [refactor]계속 실행이 되게 while(true)문 추가

--- src/main/java/numberbaseballgame/stringcalculator/view/CalculatorMain.java
+++ src/main/java/numberbaseballgame/stringcalculator/view/CalculatorMain.java
@@ -6,17 +6,20 @@ import numberbaseballgame.stringcalculator.domain.UserInputReader;
 import java.util.Scanner;
 
 public class CalculatorMain {
+    private static final String SPACE_SEPARATOR = " ";
     public static void main(String[] args) {
-        System.out.print("계산하고 싶은 문자열을 입력해주세요 > ");
-        Scanner scanner = new Scanner(System.in);
-        String inputString = scanner.nextLine();
+        while (true) {
+            System.out.print("계산하고 싶은 문자열을 입력해주세요 > ");
+            Scanner scanner = new Scanner(System.in);
+            String inputString = scanner.nextLine();
 
-        UserInputReader userInputReader = new UserInputReader();
-        userInputReader.splitBy(inputString, " ");
+            UserInputReader userInputReader = new UserInputReader();
+            userInputReader.splitBy(inputString, SPACE_SEPARATOR);
 
-        Calculator calculator = new Calculator(userInputReader);
-        int result = calculator.performCalculate();
+            Calculator calculator = new Calculator(userInputReader);
+            int result = calculator.performCalculate();
 
-        System.out.println("계산의 결과는 " + result + "입니다");
+            System.out.println("계산의 결과는 " + result + "입니다");
+        }
     }
 }

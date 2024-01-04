0c856d6c55d236b2840ce1021990c2a7c4c741d8
Hyungjun Lim <hotfirebulldog@gmail.com>
2024-01-04 17:51:25 +0900

    [test]예외테스트 작성

--- src/test/java/numberbaseballgame/stringcalculator/domain/UserInputReaderTest.java
+++ src/test/java/numberbaseballgame/stringcalculator/domain/UserInputReaderTest.java
@@ -17,6 +17,7 @@ class UserInputReaderTest {
         assertThat(userInputReader.convert(input, " ")).contains("1", "2", "+", "-");
     }
 
+    @DisplayName("입력한 문자열 중에 숫자가 아닌 다른 값을 입력했을 때의 예외처리 테스트")
     @Test
     void test_input_is_not_numberic() {
         String input = "j + 1 - 2";
@@ -24,4 +25,23 @@ class UserInputReaderTest {
             userInputReader.convert(input, " ");
         }).hasMessageContaining("숫자가 아닌 값을 입력하셨습니다");
     }
+
+    @DisplayName("입력한 문자열 중에 연산 기호가 아닌 문자를 입력했을 때의 예외처리 테스트")
+    @Test
+    void test_input_is_not_operator() {
+        String input = "1 $ 1 @ 2";
+        assertThatThrownBy(() -> {
+            userInputReader.convert(input, " ");
+        }).hasMessageContaining("연산 기호가 아닌 문자를 입력하셨습니다");
+    }
+
+    @DisplayName("입력한 문자열 중에 숫자가 10를 넘었을 때 예외처리 테스트")
+    @Test
+    void test_input_is_not_Integer() {
+        String input = "111111111111111111111111 + 19999999999999999999999999999999999999999 - 2";
+        assertThatThrownBy(() -> {
+            userInputReader.convert(input, " ");
+        }).hasMessageContaining("숫자는 10자리까지만 입력이 가능합니다");
+    }
+
 }

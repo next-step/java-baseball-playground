08e892f09a10af9cfb57e1afa5f0fc9123739c75
seonwoo_jung <laborlawseon@gmail.com>
2024-01-09 21:12:12 +0900

    init

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,26 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void replace2() {
+        String[] split = "1,2".split(",");
+        assertThat(split).isEqualTo(new String[]{"1", "2"});
+    }
+
+    @Test
+    void replace3() {
+        assertThatThrownBy(() -> {
+            String str = "abc";
+            str.charAt(3);
+        }).isInstanceOf(IndexOutOfBoundsException.class).hasMessageContaining("String index out of range: 3");
     }
 }
